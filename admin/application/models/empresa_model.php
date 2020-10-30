a2201c961860c49d53fb3246e3d11b7fc03ff107
Matias Basile <basile.matias99@gmail.com>
2020-10-30 14:21:33 -0300

    Agregue planes en el registro

--- admin/application/models/empresa_model.php
+++ admin/application/models/empresa_model.php
@@ -812,21 +812,12 @@ class Empresa_Model extends Abstract_Model {
     ));
       
     // Creamos un registro de configuracion
-    $sql = "INSERT INTO web_configuracion (id_empresa,id_proyecto,email,entradas_por_pagina,tipo_empresa,primer_login,seo_title,";
+    $sql = "INSERT INTO web_configuracion (id_web_template,id_empresa,id_proyecto,email,entradas_por_pagina,tipo_empresa,primer_login,seo_title,";
     $sql.= " template_header,template_footer,template_home,template_contacto,template_productos_detalle,template_productos_listado,template_noticias_detalle,template_noticias_listado,template_pagina,color_fondo_imagenes_defecto, ";
     $sql.= " clienapp_abierto, clienapp_posicion, clienapp_sonido, tienda_registro_direccion, tienda_registro_telefono, crm_notificar_asignaciones_usuarios, crm_notificar_tareas, tienda_registro_ciudad ";
-    // Shopvar
-    if ($array->id_proyecto == 2) $sql.= ",id_punto_venta ";
-    // Clienapp
-    $sql.= ",clienapp_prefijo,clienapp_largo_telefono ";
-    
-    $sql.= ") VALUES ('$id_empresa','$array->id_proyecto','$array->email',16,'$tipo_empresa',1,'$array->nombre',";
+    $sql.= ") VALUES (33,'$id_empresa','$array->id_proyecto','$array->email',16,'$tipo_empresa',1,'$array->nombre',";
     $sql.= "'header','footer','home','contacto','productos_detalle','productos_listado','noticias_detalle','noticias_listado','pagina','rgba(255,255,255,1)', ";
     $sql.= " 1, 'D', 1, 1, 1, 1, 1, 1 ";
-    // Shopvar
-    if ($array->id_proyecto == 2) $sql.= ",$id_punto_venta_1 ";
-    // Clienapp
-    $sql.= ",'549','10' ";
     $sql.= ")";
     $this->db->query($sql);
 
