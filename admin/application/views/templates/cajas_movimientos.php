edfaa2e347094040003baeb9af739869b3c8d1f5
Matias Basile <basile.matias99@gmail.com>
2020-11-17 00:09:35 -0300

    cambios

--- admin/application/views/templates/cajas_movimientos.php
+++ admin/application/views/templates/cajas_movimientos.php
@@ -1,169 +1,161 @@
 <script type="text/template" id="listado_cajas_movimientos_panel_template">
-  <div class="bg-light lter b-b wrapper-md ng-scope">
-    <div class="row clearfix padder">
-      <% if (ver_saldos == 0) { %>
-        <h1 class="m-n font-thin h3 pull-left"><i class="fa fa-shopping-cart icono_principal"></i><%= (tipo==0)?"Ingresos":"Compras / <b>Egresos</b>" %></h1>
-      <% } else { %>
-        <% var modulo = control.get("cajas") %>
-        <h1 class="m-n font-thin h3 pull-left"><a style="color:inherit" href="app/#cajas"><i class="<%= modulo.clase %> icono_principal"></i><%= modulo.nombre_es %></a>
-          / <b><%= titulo %></b>
-        </h1>
-      <% } %>
+<div class="centrado rform">
+  <div class="header-lg">
+    <div class="row">
+      <div class="col-md-6 col-xs-8">
+        <h1>Cajas</h1>
+      </div>
+      <div class="col-md-6 col-xs-4 tar">
+        <a class="btn btn-info btn-addon" href="app/#caja"><i class="fa fa-plus"></i>&nbsp;&nbsp;Nueva Caja&nbsp;&nbsp;</a>
+      </div>
     </div>
   </div>
-  <div class="wrapper-md ng-scope">
-    <div class="panel panel-default">
 
-      <% if (tipo == 1) { %>
-        <?php $active = "gastos"; include("compras/compras_menu.php"); ?>
-      <% } else { %>
-        <?php $active = "cajas"; include("caja/caja_menu.php"); ?>
-      <% } %>
+  <div class="panel panel-default">
 
-      <div class="panel-heading clearfix">
-        <div class="row">
-          <div class="col-md-6 col-lg-5 sm-m-b">
-            <div class="form-inline">    
-              <div class="input-group" style="width: 140px;">
-                <input type="text" placeholder="Desde" id="cajas_movimientos_desde" class="form-control">
-                <span class="input-group-btn">
-                  <button tabindex="-1" type="button" class="btn btn-default btn-cal"><i class="glyphicon glyphicon-calendar"></i></button>
-                </span>              
-              </div>
-              <div class="input-group" style="width: 140px;">
-                <input type="text" placeholder="Hasta" id="cajas_movimientos_hasta" class="form-control">
-                <span class="input-group-btn">
-                  <button tabindex="-1" type="button" class="btn btn-default btn-cal"><i class="glyphicon glyphicon-calendar"></i></button>
-                </span>              
-              </div>
-              <?php /*
-              <div class="form-group">
-                <button class="btn buscar btn-default"><i class="fa fa-search"></i></button>
-              </div>
-              */ ?>
-              <div class="form-group">
-                <button class="btn btn-default advanced-search-btn"><i class="fa fa-filter mr5"></i> Filtros</button>
-              </div>
+    <div class="panel-heading clearfix">
+      <div class="row">
+        <div class="col-md-6 col-lg-5 sm-m-b">
+          <div class="form-inline">    
+            <div class="input-group" style="width: 140px;">
+              <input type="text" placeholder="Desde" id="cajas_movimientos_desde" class="form-control">
+              <span class="input-group-btn">
+                <button tabindex="-1" type="button" class="btn btn-default btn-cal"><i class="glyphicon glyphicon-calendar"></i></button>
+              </span>              
+            </div>
+            <div class="input-group" style="width: 140px;">
+              <input type="text" placeholder="Hasta" id="cajas_movimientos_hasta" class="form-control">
+              <span class="input-group-btn">
+                <button tabindex="-1" type="button" class="btn btn-default btn-cal"><i class="glyphicon glyphicon-calendar"></i></button>
+              </span>              
+            </div>
+            <?php /*
+            <div class="form-group">
+              <button class="btn buscar btn-default"><i class="fa fa-search"></i></button>
+            </div>
+            */ ?>
+            <div class="form-group">
+              <button class="btn btn-default advanced-search-btn"><i class="fa fa-filter mr5"></i> Filtros</button>
             </div>
           </div>
-          <div class="col-md-6 col-lg-7 sm-m-b">
+        </div>
+        <div class="col-md-6 col-lg-7 sm-m-b">
 
+          <div class="btn-group pull-right dropdown ml5">
+            <button class="btn btn-default dropdown-toggle btn-addon" data-toggle="dropdown">
+              <i class="fa fa-cog"></i><span>Opciones</span>
+              <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu">
+              <li><a href="javascript:void(0)" class="transferencia">Transferencia</a></li>
+              <li><a href="javascript:void(0)" class="exportar">Exportar Excel</a></li>
+              <% if (VOLVER_SUPERADMIN == 1) { %>
+                <li><a href="javascript:void(0)" class="nuevo_ajuste">Ajuste de Caja</a></li>
+              <% } %>
+            </ul>
+          </div>
+
+          <% if (ver_saldos == 0) { %>
+            <a class="btn pull-right btn-info btn-addon ml5 nuevo_caja_movimiento" href="javascript:void(0)">
+              <i class="fa fa-plus"></i><span>&nbsp;&nbsp;<?php echo lang(array("es"=>"Nuevo","en"=>"New")); ?>&nbsp;&nbsp;</span>
+            </a>
+          <% } else { %>
             <div class="btn-group pull-right dropdown ml5">
-              <button class="btn btn-default dropdown-toggle btn-addon" data-toggle="dropdown">
-                <i class="fa fa-cog"></i><span>Opciones</span>
+              <button class="btn btn-danger dropdown-toggle btn-addon" data-toggle="dropdown">
+                <i class="fa fa-arrow-down"></i><span>&nbsp;&nbsp;<?php echo lang(array("es"=>"Egreso","en"=>"Exit")); ?>&nbsp;&nbsp;</span>
                 <span class="caret"></span>
               </button>
               <ul class="dropdown-menu">
-                <li><a href="javascript:void(0)" class="transferencia">Transferencia</a></li>
-                <li><a href="javascript:void(0)" class="exportar">Exportar Excel</a></li>
-                <% if (VOLVER_SUPERADMIN == 1) { %>
-                  <li><a href="javascript:void(0)" class="nuevo_ajuste">Ajuste de Caja</a></li>
+                <li><a href="javascript:void(0)" class="nuevo_gasto">Efectivo</a></li>
+                <% if (ID_EMPRESA != 249) { %>
+                  <li><a href="javascript:void(0)" class="nuevo_egreso_cheque">Cheque</a></li>
                 <% } %>
               </ul>
             </div>
-
-            <% if (ver_saldos == 0) { %>
-              <a class="btn pull-right btn-info btn-addon ml5 nuevo_caja_movimiento" href="javascript:void(0)">
-                <i class="fa fa-plus"></i><span>&nbsp;&nbsp;<?php echo lang(array("es"=>"Nuevo","en"=>"New")); ?>&nbsp;&nbsp;</span>
-              </a>
-            <% } else { %>
-              <div class="btn-group pull-right dropdown ml5">
-                <button class="btn btn-danger dropdown-toggle btn-addon" data-toggle="dropdown">
-                  <i class="fa fa-arrow-down"></i><span>&nbsp;&nbsp;<?php echo lang(array("es"=>"Egreso","en"=>"Exit")); ?>&nbsp;&nbsp;</span>
-                  <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu">
-                  <li><a href="javascript:void(0)" class="nuevo_gasto">Efectivo</a></li>
-                  <% if (ID_EMPRESA != 249) { %>
-                    <li><a href="javascript:void(0)" class="nuevo_egreso_cheque">Cheque</a></li>
-                  <% } %>
-                </ul>
-              </div>
-              <div class="btn-group pull-right dropdown ml5">
-                <button class="btn btn-success dropdown-toggle btn-addon" data-toggle="dropdown">
-                  <i class="fa fa-arrow-up"></i><span>&nbsp;&nbsp;<?php echo lang(array("es"=>"Ingreso","en"=>"Income")); ?>&nbsp;&nbsp;</span>
-                  <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu">
-                  <li><a href="javascript:void(0)" class="nuevo_ingreso">Efectivo</a></li>
-                  <% if (ID_EMPRESA != 249) { %>
-                    <li><a href="javascript:void(0)" class="nuevo_ingreso_cheque">Cheque</a></li>
-                  <% } %>
-                </ul>
-              </div>
-            <% } %>
-
-          </div>
-        </div>
-      </div>
-      <% var display_search = (id_concepto != 0) ? "display:block":"display:none" %>
-      <div class="advanced-search-div bg-light dk" style="<%= display_search %>">
-        <div class="wrapper clearfix">
-          <h4 class="m-t-xs"><i class="fa fa-search"></i> B&uacute;squeda Avanzada:</h4>
-          <div class="form-inline">    
-            <div class="form-group">
-              <select class="form-control no-model" id="cajas_movimientos_conceptos">
-                <option value="0">Concepto</option>
-                <%= workspace.crear_select(tipos_gastos,"",id_concepto) %>
-              </select>
-            </div>            
-            <div class="form-group">
-              <select class="form-control no-model" id="cajas_movimientos_estado">
-                <option <%= (estado==-1)?"selected":"" %> value="-1">Estado</option>
-                <option <%= (estado==0)?"selected":"" %> value="0">Realizado</option>
-                <option <%= (estado==1)?"selected":"" %> value="1">Pendiente</option>
-              </select>
-            </div>            
-            <div class="form-group">
-              <select class="form-control no-model" id="cajas_movimientos_orden_pago">
-                <option <%= (orden_pago==-1)?"selected":"" %> value="-1">Orden de Pago</option>
-                <option <%= (orden_pago==1)?"selected":"" %> value="1">Pertenece a OP</option>
-                <option <%= (orden_pago==0)?"selected":"" %> value="0">No pertenece a OP</option>
-              </select>
-            </div>                        
-          </div>
-        </div>
-      </div>
-
-      <div class="bulk_action panel-body resumen pb0">
-        <div class="row">
-          <div class="col-md-3">
-            <div class="block tac panel padder-v item bg-success mb0" style="height: 80px">
-              <div id="cajas_movimientos_monto" class="h3 font-thin text-white block">0</div>
-              <span class="text-muted text-md pt5 db">Total</span>
+            <div class="btn-group pull-right dropdown ml5">
+              <button class="btn btn-success dropdown-toggle btn-addon" data-toggle="dropdown">
+                <i class="fa fa-arrow-up"></i><span>&nbsp;&nbsp;<?php echo lang(array("es"=>"Ingreso","en"=>"Income")); ?>&nbsp;&nbsp;</span>
+                <span class="caret"></span>
+              </button>
+              <ul class="dropdown-menu">
+                <li><a href="javascript:void(0)" class="nuevo_ingreso">Efectivo</a></li>
+                <% if (ID_EMPRESA != 249) { %>
+                  <li><a href="javascript:void(0)" class="nuevo_ingreso_cheque">Cheque</a></li>
+                <% } %>
+              </ul>
             </div>
+          <% } %>
+
+        </div>
+      </div>
+    </div>
+    <% var display_search = (id_concepto != 0) ? "display:block":"display:none" %>
+    <div class="advanced-search-div bg-light dk" style="<%= display_search %>">
+      <div class="wrapper clearfix">
+        <h4 class="m-t-xs"><i class="fa fa-search"></i> B&uacute;squeda Avanzada:</h4>
+        <div class="form-inline">    
+          <div class="form-group">
+            <select class="form-control no-model" id="cajas_movimientos_conceptos">
+              <option value="0">Concepto</option>
+              <%= workspace.crear_select(tipos_gastos,"",id_concepto) %>
+            </select>
+          </div>            
+          <div class="form-group">
+            <select class="form-control no-model" id="cajas_movimientos_estado">
+              <option <%= (estado==-1)?"selected":"" %> value="-1">Estado</option>
+              <option <%= (estado==0)?"selected":"" %> value="0">Realizado</option>
+              <option <%= (estado==1)?"selected":"" %> value="1">Pendiente</option>
+            </select>
+          </div>            
+          <div class="form-group">
+            <select class="form-control no-model" id="cajas_movimientos_orden_pago">
+              <option <%= (orden_pago==-1)?"selected":"" %> value="-1">Orden de Pago</option>
+              <option <%= (orden_pago==1)?"selected":"" %> value="1">Pertenece a OP</option>
+              <option <%= (orden_pago==0)?"selected":"" %> value="0">No pertenece a OP</option>
+            </select>
+          </div>                        
+        </div>
+      </div>
+    </div>
+
+    <div class="bulk_action panel-body resumen pb0">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="block tac panel padder-v item bg-success mb0" style="height: 80px">
+            <div id="cajas_movimientos_monto" class="h3 font-thin text-white block">0</div>
+            <span class="text-muted text-md pt5 db">Total</span>
           </div>
-          <div class="col-md-3">
-            <div class="block tac panel padder-v item bg-info mb0" style="height: 80px">
-              <span id="cajas_movimientos_cantidad" class="font-thin h3 block">0</span>
-              <span class="text-muted text-md pt5 db">Operaciones</span>
-            </div>
+        </div>
+        <div class="col-md-3">
+          <div class="block tac panel padder-v item bg-info mb0" style="height: 80px">
+            <span id="cajas_movimientos_cantidad" class="font-thin h3 block">0</span>
+            <span class="text-muted text-md pt5 db">Operaciones</span>
           </div>
         </div>
       </div>
+    </div>
 
-      <div class="panel-body">
-        <div class="b-a table-responsive">
-          <table id="cajas_movimientos_tabla" class="table table-striped sortable m-b-none default footable">
-            <thead>
-              <tr>
-                <th style="width:20px;">
-                  <label class="i-checks m-b-none">
-                    <input class="esc sel_todos" type="checkbox"><i></i>
-                  </label>
-                </th>
-                <th class="w150">Estado</th>
-                <th class="w180 exportable">Fecha</th>
-                <th class="exportable">Concepto</th>
-                <th class="exportable">Descripci&oacute;n</th>
-                <th class="exportable tar w150">Monto</th>
-                <th class="exportable tar w150">Saldo</th>
-                <th style="width:20px;"></th>
-              </tr>
-            </thead>
-            <tbody></tbody>
-          </table>
-        </div>
+    <div class="panel-body">
+      <div class="b-a table-responsive">
+        <table id="cajas_movimientos_tabla" class="table table-striped sortable m-b-none default footable">
+          <thead>
+            <tr>
+              <th style="width:20px;">
+                <label class="i-checks m-b-none">
+                  <input class="esc sel_todos" type="checkbox"><i></i>
+                </label>
+              </th>
+              <th class="w150">Estado</th>
+              <th class="w180 exportable">Fecha</th>
+              <th class="exportable">Concepto</th>
+              <th class="exportable">Descripci&oacute;n</th>
+              <th class="exportable tar w150">Monto</th>
+              <th class="exportable tar w150">Saldo</th>
+              <th style="width:20px;"></th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
       </div>
     </div>
   </div>
