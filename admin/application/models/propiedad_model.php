dc20e579ecbd0c6215c646173e8e798e6c6f52f9
Matias Basile <basile.matias99@gmail.com>
2020-12-01 21:22:28 -0300

    agregue direccion_completa en todos los templates

--- admin/application/models/propiedad_model.php
+++ admin/application/models/propiedad_model.php
@@ -655,8 +655,8 @@ class Propiedad_Model extends Abstract_Model {
       }
 
       $r->titulo = $this->generar_titulo($r);
-      $direccion = ($r->calle. (($r->publica_altura == 1)?" ".$r->altura:"") . (!empty($r->piso) ? " Piso ".$r->piso : "") . (!empty($r->numero) ? " Depto. ".$r->numero : ""));
-      $r->direccion_completa = $direccion.(!empty($r->entre_calles) ? " e/ ".$r->entre_calles.(!empty($r->entre_calles_2) ? " y ".$r->entre_calles_2 : "") : "");
+      $r->direccion_completa = $r->calle.(!empty($r->entre_calles) ? " e/ ".$r->entre_calles.(!empty($r->entre_calles_2) ? " y ".$r->entre_calles_2 : "") : "");
+      $r->direccion_completa.= (($r->publica_altura == 1)?" N° ".$r->altura:"") . (!empty($r->piso) ? " Piso ".$r->piso : "") . (!empty($r->numero) ? " Depto. ".$r->numero : "");
       /*
       if ($filtro_meli == 1 && $r->status != "active") $ingresar_row = 0;
       else if ($filtro_meli == 2 && $r->status != "paused") $ingresar_row = 0;
@@ -1105,8 +1105,9 @@ class Propiedad_Model extends Abstract_Model {
     }
 
     $propiedad->titulo = $this->generar_titulo($propiedad);
-    $direccion = ($propiedad->calle. (($propiedad->publica_altura == 1)?" ".$propiedad->altura:"") . (!empty($propiedad->piso) ? " Piso ".$propiedad->piso : "") . (!empty($propiedad->numero) ? " Depto. ".$propiedad->numero : ""));
-    $propiedad->direccion_completa = $direccion.(!empty($propiedad->entre_calles) ? " e/ ".$propiedad->entre_calles.(!empty($propiedad->entre_calles_2) ? " y ".$propiedad->entre_calles_2 : "") : "");
+
+    $propiedad->direccion_completa = $propiedad->calle.(!empty($propiedad->entre_calles) ? " e/ ".$propiedad->entre_calles.(!empty($propiedad->entre_calles_2) ? " y ".$propiedad->entre_calles_2 : "") : "");
+    $propiedad->direccion_completa.= (($propiedad->publica_altura == 1)?" N° ".$propiedad->altura:"") . (!empty($propiedad->piso) ? " Piso ".$propiedad->piso : "") . (!empty($propiedad->numero) ? " Depto. ".$propiedad->numero : "");
 
     return $propiedad;
   }
--- models/Propiedad_Model.php
+++ models/Propiedad_Model.php
@@ -814,8 +814,9 @@ class Propiedad_Model {
       $p->precio = "Consultar";
     }
 
-    $p->direccion = $this->encod($p->calle. (($p->publica_altura == 1)?" ".$p->altura:"") . (!empty($p->piso) ? " Piso ".$p->piso : "") . (!empty($p->numero) ? " Depto. ".$p->numero : ""));
-    $p->direccion_completa = $p->direccion.(!empty($p->entre_calles) ? " e/ ".$p->entre_calles.(!empty($p->entre_calles_2) ? " y ".$p->entre_calles_2 : "") : "");
+    $p->direccion_completa = (!empty($p->entre_calles) ? " e/ ".$p->entre_calles.(!empty($p->entre_calles_2) ? " y ".$p->entre_calles_2 : "") : "");
+    $p->direccion_completa.= (($p->publica_altura == 1)?" N° ".$p->altura:"") . (!empty($p->piso) ? " Piso ".$p->piso : "") . (!empty($p->numero) ? " Depto. ".$p->numero : "");
+    $p->direccion_completa = $this->encod($p->direccion_completa);
 
     $p->plain_text = (!empty($p->descripcion)) ? $this->encod($p->descripcion) : $this->encod(strip_tags($p->texto,"<a><i><b><br>"));
 
