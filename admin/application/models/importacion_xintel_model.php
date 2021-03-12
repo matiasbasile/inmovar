e3df04ca022356c345dcc77f5e950794321fadcc
Matias Basile <basile.matias99@gmail.com>
2021-03-12 16:02:31 -0300

    Scrapper xintel

--- admin/application/models/importacion_xintel_model.php
+++ admin/application/models/importacion_xintel_model.php
@@ -34,7 +34,7 @@ class Importacion_Xintel_Model extends Abstract_Model {
     
     foreach ($result->resultado->fichas as $p) {
 
-      $sql = "SELECT * FROM inm_propiedades WHERE codigo = '$p->in_num' AND id_empresa = '$id_empresa' ";
+      $sql = "SELECT id FROM inm_propiedades WHERE codigo = '$p->in_num' AND id_empresa = '$id_empresa' ";
       $q = $this->db->query($sql);
       
       if ($q->num_rows()==0) {
@@ -77,8 +77,12 @@ class Importacion_Xintel_Model extends Abstract_Model {
         $sql.= "dormitorios, cocheras, banios, ambientes, superficie_cubierta, superficie_total, superficie_descubierta, superficie_semicubierta, calle, numero, activo, compartida) ";
         $sql.= "VALUES ('$p->in_num', '$obs', '$codigo', $id_empresa, $tipo_inmueble, $tipo, 1, '$p->alquiler_moneda', '$p->in_val', '$path', '$latitud', '$longitud', ";
         $sql.= "'$p->dormitorios', '$p->in_coc', '$p->in_ban', '$p->ambientes_num', '$p->in_m01', '$p->in_sto', '$p->in_m02', '$p->in_m03', '$p->in_cal', '$p->in_nro', 1, 1); ";
-
         $this->db->query($sql);
+
+
+        //TODO: HACER CURL DE CADA PROPIEDAD PARA TRAER LAS IMAGENES
+        //https://xintel.com.ar/api/?cache=12032021&json=fichas.propiedades&amaira=false&suc=JYB&global=LU3AIKPR4F6ZSUY8GQODKWRO8&emprendimiento=True&oppel=&esweb=&apiK=4m17zq256jvsm24wOnqbev43y&id=696&_=1615575453648
+
       } else {
         if ($p->operacion == "Venta"){
           $tipo = 1;
@@ -96,6 +100,8 @@ class Importacion_Xintel_Model extends Abstract_Model {
         $sql.= "superficie_semicubierta = '$p->in_m03', calle = '$p->in_cal', numero = '$p->in_nro', activo = 1, compartida = 1 ";
         $sql.= "WHERE id_empresa = $id_empresa AND codigo = '$p->in_num' ";
         $this->db->query($sql);
+        //TODO: HACER CURL DE CADA PROPIEDAD PARA TRAER LAS IMAGENES
+        //https://xintel.com.ar/api/?cache=12032021&json=fichas.propiedades&amaira=false&suc=JYB&global=LU3AIKPR4F6ZSUY8GQODKWRO8&emprendimiento=True&oppel=&esweb=&apiK=4m17zq256jvsm24wOnqbev43y&id=696&_=1615575453648
       }
       $x++;
     }
