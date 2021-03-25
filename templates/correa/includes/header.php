7387f9b5f4f6b7b8147631193056d691c7367e99
Mati F <matiasfittipaldi@varcreative.com>
2021-03-25 14:36:28 -0300

    Alt Descripcion

--- templates/correa/includes/header.php
+++ templates/correa/includes/header.php
@@ -40,7 +40,7 @@
 </div>
 <header class="<?php echo ($page_act=="home")?"home-header":""?>">  
 	<nav class="navbar navbar-expand-lg">
-		<a class="navbar-brand" href="<?php echo mklink ("/") ?>"><img src="assets/images/logo.png" alt="Lasa Papelera"></a>
+		<a class="navbar-brand" href="<?php echo mklink ("/") ?>"><img src="assets/images/logo.png" alt="<?php echo $empresa->nombre ?>"></a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
