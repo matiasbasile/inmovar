16817cb55d4d3d09dd47bb3d128f66f874b431eb
matiasbasile <basilematias99@gmail.com>
2021-04-24 16:26:10 -0300

    importar tokko ahora usa propiedad_model save

--- admin/application/models/propiedad_model.php
+++ admin/application/models/propiedad_model.php
@@ -816,12 +816,12 @@ class Propiedad_Model extends Abstract_Model {
     // Si en el campo calle tiene un e/ o entre, tenemos que sacarlo
     if (strpos(mb_strtolower($data->calle), "e/")>0) {
       $entre = substr($data->calle, strpos(mb_strtolower($data->calle), "e/"));
-      $entre = str_replace("e/", "", $entre);
       if (strpos($entre, "y")>0) {
         $e = explode("y", $entre);
         if (sizeof($e)==2) {
           $data->calle = str_replace($entre, "", $data->calle);
           $entre_calles = trim($e[0]);
+          $entre_calles = str_replace("e/", "", $entre_calles);
           $entre_calles_2 = trim($e[1]);
           $data->entre_calles = ucwords($entre_calles);
           $data->entre_calles_2 = ucwords($entre_calles_2);
@@ -830,12 +830,12 @@ class Propiedad_Model extends Abstract_Model {
     }
     if (strpos(mb_strtolower($data->calle), "entre")>0) {
       $entre = substr($data->calle, strpos(mb_strtolower($data->calle), "entre"));
-      $entre = str_replace("entre", "", $entre);
       if (strpos($entre, "y")>0) {
         $e = explode("y", $entre);
         if (sizeof($e)==2) {
           $data->calle = str_replace($entre, "", $data->calle);
           $entre_calles = trim($e[0]);
+          $entre_calles = str_replace("entre", "", $entre_calles);
           $entre_calles_2 = trim($e[1]);
           $data->entre_calles = ucwords($entre_calles);
           $data->entre_calles_2 = ucwords($entre_calles_2);
