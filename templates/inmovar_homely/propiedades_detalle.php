040f5783c2c1de57256ddb81fa492e473cf0b768
matiasbasile <basilematias99@gmail.com>
2021-05-08 12:09:38 -0300

    cambios

--- templates/inmovar_homely/propiedades_detalle.php
+++ templates/inmovar_homely/propiedades_detalle.php
@@ -2,8 +2,6 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$propiedad = $propiedad_model->get($id);
-extract($propiedad_model->get_variables());
 
 $id_empresa = isset($get_params["em"]) ? $get_params["em"] : $empresa->id;
 $propiedad = $propiedad_model->get($id,array(
