7ef8817281e7483fea62aca2544ff6c0e3a82604
matiasbasile <basilematias99@gmail.com>
2021-06-11 13:48:05 -0300

    cambios

--- templates/buscador/assets/css/custom.php
+++ templates/buscador/assets/css/custom.php
@@ -6117,17 +6117,19 @@ input[type="checkbox"]:checked + label:before, input[type="radio"]:checked + lab
 }
 
 .buscador-flotante, .operaciones-flotante { display: none; background-color: #f3f3f3; padding: 10px; }
-.cerrar_filtros, #orden_propiedades { display: none; }
+.cerrar_filtros, #orden_propiedades, #form_flotante { display: none; }
 input.form-control { box-shadow: 0px 1px 1px rgb(0 0 0 / 20%); }
 #search-filter .sorting select { width: auto; }
 #search-filter .sorting label { margin-right: 10px; }
 @media (max-width: 768px) {
+  #contacto_submit { display: block; width: 100%; }
   .form-search { display: none; }
   .navbar-brand { float: none; }
   #search-filter { margin-bottom: 20px; }
   .page-search-results .navigation .navbar { text-align: center; }
   .page-search-results .sorting { display: none; }
   .page-search-results .buscador-flotante, .page-search-results .operaciones-flotante { display: block; position: fixed; width: 100%; bottom: 0px; left: 0px; right: 0px; z-index: 9998; }
+  .page-property-detail .buscador-flotante { display: none !important }
   .page-search-results .buscador-flotante .button, .page-search-results .operaciones-flotante .button { border: solid 2px <?php echo $c1; ?>; color: #5a5a5a; font-size: 15px; padding: 10px 9px 9px 9px; outline: none; width: 100%; text-align: center; border-radius: 10px; font-weight: bold; }
   .page-search-results .buscador-flotante .button:hover, .page-search-results .operaciones-flotante .button:hover, 
   .page-search-results .buscador-flotante .button:focus, .page-search-results .operaciones-flotante .button:focus, 
