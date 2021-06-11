1432f569379eb3ec93488b9b9f24595b95ed88fd
matiasbasile <basilematias99@gmail.com>
2021-06-11 20:44:13 -0300

    cambios

--- templates/buscador/assets/css/custom.php
+++ templates/buscador/assets/css/custom.php
@@ -6128,7 +6128,7 @@ input.form-control { box-shadow: 0px 1px 1px rgb(0 0 0 / 20%); }
   .form-search { display: none; }
   .navbar-brand { float: none; }
   #search-filter { margin-bottom: 20px; }
-  .btn-whatsapp { background-color:#20be3a; border-color:#20be3a; color: white; font-size: 18px; }
+  .btn-whatsapp { background-color: #20be3a !important; border-color: #20be3a !important; color: white !important; font-size: 21px !important; padding: 5px !important; }
   .page-search-results .btn-limpiar { border-radius: 25px; font-size: 13px; display:inline-block; background-color:#dddddd;color:#5a5a5a; cursor:pointer; padding:4px 10px; }
   .page-search-results .navigation .navbar { text-align: center; }
   .page-search-results .sorting { display: none; }
