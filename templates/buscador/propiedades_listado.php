d05ee779cd56ca720cdf041da4fe7bfdbcf3fffa
matiasbasile <basilematias99@gmail.com>
2021-06-11 13:19:05 -0300

    cambios

--- templates/buscador/propiedades_listado.php
+++ templates/buscador/propiedades_listado.php
@@ -56,7 +56,11 @@ if (isset($_GET["test"])) print_r($vc_listado);
                     <?php } else if ($r->id_tipo_estado == 3) { ?>
                       <figure class="ribbon">Vendido</figure>
                     <?php } ?>
-                    <figure class="ribbon2"><img src="assets/img/red.png" alt="red inmovar" /></figure>
+                    <figure class="ribbon2">
+                      <a href="<?php echo $r->link_propiedad; ?>">
+                        <img src="assets/img/red.png" alt="red inmovar" />
+                      </a>
+                    </figure>
                     <a href="<?php echo $r->link_propiedad; ?>">
                       <?php if (!empty($r->imagen)) { ?>
                         <img loading="lazy" src="<?php echo $r->imagen ?>" alt="<?php echo ($r->nombre); ?>" />
