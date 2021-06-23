0006effae38ed2ef25fb40993aeba7f1ecdf1f19
matiasbasile <basilematias99@gmail.com>
2021-06-23 12:02:18 -0300

    cambios

--- index.php
+++ index.php
@@ -222,10 +222,10 @@ if ($empresa->administrar_pagos == 1 && $fecha_suspension->format("Y-m-d") < dat
 
 $dominio = "http://".$dominio;
 $nombre_pagina = (sizeof($params)>0) ? $params[0] : "";
-
+echo "[$nombre_pagina]";
 if ($nombre_pagina == "ficha") {
   include("templates/ficha/home.php");
-  
+
 } else if (isset($empresa->template_path) && !empty($empresa->template_path)) { 
 
   $dir_template = "templates/$empresa->template_path/";
