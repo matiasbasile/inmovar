5491b5cefae481ec1495095afa76970afe9ec56a
matiasbasile <basilematias99@gmail.com>
2021-07-20 23:54:08 -0300

    stories

--- admin/application/javascript/modules/stories.js
+++ admin/application/javascript/modules/stories.js
@@ -34,26 +34,8 @@ app.views.StoriesView = app.mixins.View.extend({
     let stories = new Zuck("stories",{
       "stories": self.model.get("stories"),
       "template": {
-        timelineStoryItem (itemData) {
-          const reserved = ['id', 'seen', 'src', 'link', 'linkText', 'time', 'type', 'length', 'preview'];
-          let attributes = `
-            href="${get(itemData, 'src')}"
-            data-link="${get(itemData, 'link')}"
-            data-linkText="${get(itemData, 'linkText')}"
-            data-time="${get(itemData, 'time')}"
-            data-type="${get(itemData, 'type')}"
-            data-length="${get(itemData, 'length')}"
-          `;
-
-          for (const dataKey in itemData) {
-            if (reserved.indexOf(dataKey) === -1) {
-              attributes += ` data-${dataKey}="${itemData[dataKey]}"`;
-            }
-          }
-
-          return `<a ${attributes}>
-                    <img loading="auto" src="${get(itemData, 'preview')}" />
-                  </a>`;
+        timelineStoryItem: function (itemData) {
+          console.log(itemData);
         },
       },
     });
