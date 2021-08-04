59a9f490eee2eced4469b07b873b526b3beb6932
Mati F <matiasfittipaldi@varcreative.com>
2021-08-04 10:16:42 -0300

    update cache css

--- templates/alaro/includes_nuevo/head.php
+++ templates/alaro/includes_nuevo/head.php
@@ -1,8 +1,8 @@
 <?php include ("templates/comun/pre_head.php") ?>
 <meta charset="utf-8">
-<link href="assets_nuevo/css/slider.css" type="text/css" media="all" rel="stylesheet" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="assets_nuevo/css/bootstrap.min.css" media="all">
-<link rel="stylesheet" type="text/css" href="assets_nuevo/css/swiper.min.css" media="all">
-<link href="assets_nuevo/css/style.css" type="text/css" media="all" rel="stylesheet" />
+<link href="assets_nuevo/css/slider.css?v=1" type="text/css" media="all" rel="stylesheet" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css?v=1">
+<link rel="stylesheet" type="text/css" href="assets_nuevo/css/bootstrap.min.css?v=1" media="all">
+<link rel="stylesheet" type="text/css" href="assets_nuevo/css/swiper.min.css?v=1" media="all">
+<link href="assets_nuevo/css/style.css?v=1" type="text/css" media="all" rel="stylesheet" />
 <?php include ("templates/comun/post_head.php") ?>
--- templates/alaro/includes_nuevo/head_new.php
+++ templates/alaro/includes_nuevo/head_new.php
@@ -2,12 +2,12 @@
 <meta charset="UTF-8">
 <meta name="author" content="Alaro">
 <!-- External CSS -->
-<link href="css/correa/owl.carousel.min.css" rel="stylesheet" type="text/css" media="all">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-<link href="css/correa/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
-<link href="css/correa/slick-theme.css" rel="stylesheet" type="text/css">
-<link href="css/correa/styles.css" rel="stylesheet" type="text/css" media="all">
-<link href="css/correa/responsive.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/correa/owl.carousel.min.css?v=1" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css?v=1">
+<link href="css/correa/bootstrap.min.css?v=1" rel="stylesheet" type="text/css" media="all">
+<link href="css/correa/slick-theme.css?v=1" rel="stylesheet" type="text/css">
+<link href="css/correa/styles.css?v=1" rel="stylesheet" type="text/css" media="all">
+<link href="css/correa/responsive.css?v=1" rel="stylesheet" type="text/css" media="all">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
