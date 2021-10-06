a352ca08158bd75d9fbb4d8f3e72474e6d8e1961
matiasbasile <basilematias99@gmail.com>
2021-10-06 19:22:15 -0300

    cmabios

--- index.php
+++ index.php
@@ -275,9 +275,10 @@ $nombre_pagina = (sizeof($params)>0) ? $params[0] : "";
 
 if ( $nombre_pagina == "ficha") {
   include_once("models/Propiedad_Model.php");
-  $propiedad_model = new Propiedad_Model($propiedad->id_empresa,$conx);
+  $propiedad_model = new Propiedad_Model($empresa->id,$conx);
   $propiedad = $propiedad_model->get(0,array(
     "hash"=>$hash,
+    "id_empresa"=>$propiedad->id_empresa,
     "buscar_imagenes"=>1,
   ));
   $id = $propiedad->id;
