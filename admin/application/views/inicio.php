087e824499b11ac8e293d4ed9492c2bd1716205e
matiassctt <poppydch@gmail.com>
2021-11-01 11:10:33 -0300

    SW comentado

--- admin/application/views/inicio.php
+++ admin/application/views/inicio.php
@@ -69,14 +69,14 @@
 
 <script type="text/javascript">
 
-
+/*
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw_app.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
-} 
+}*/
 
-  
+
 jQuery(document).ready(function($) {
   $("#nombre").focus();
   $(".input").keypress(function(e){
