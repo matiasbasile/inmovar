d1c1dd6096e9130543148739263d478d185a4ff5
matiasbasile <basilematias99@gmail.com>
2022-01-21 17:00:08 -0300

    cambios

--- templates/grupourbano2/home.php
+++ templates/grupourbano2/home.php
@@ -251,20 +251,20 @@ include_once("includes/funciones.php");
   <?php } ?>
 
   <?php
-  $countries = $propiedad_model->get_list(array("id_tipo_inmueble" => 4, "limit" => 0, "offset" => 6));
+  $obras = $propiedad_model->get_list(array("id_tipo_operacion" => 5, "limit" => 0, "offset" => 6));
   ?>
   <!-- Work -->
-  <?php if (!empty($countries)) { ?>
+  <?php if (!empty($obras)) { ?>
     <section class="padding-default works">
       <div class="container text-center">
-        <?php $t = $web_model->get_text("text15", "Especial Countries y Barrios Cerrados"); ?>
+        <?php $t = $web_model->get_text("text15", "Especial Obras en Construcción"); ?>
         <h2 class="editable" data-id="<?php echo $t->id ?>" data-id_empresa="<?php echo $t->id_empresa ?>" data-clave="<?php echo $t->clave ?>"> <?php echo $t->plain_text ?></h2>
 
         <?php $t = $web_model->get_text("text16", "Disfrutá de la seguridad y privacidad de vivir en un barrio cerrado y/o controlado."); ?>
         <h5 class="editable" data-id="<?php echo $t->id ?>" data-id_empresa="<?php echo $t->id_empresa ?>" data-clave="<?php echo $t->clave ?>"> <?php echo $t->plain_text ?></h5>
       </div>
       <div class="row m-0 mt-5 pad-lr-50">
-        <?php foreach ($countries as $countri) { ?>
+        <?php foreach ($obras as $countri) { ?>
           <div class="col-md-3 work-list">
             <div class="img-block">
               <a href="<?php echo mklink($countri->link) ?>" class="stretched-link"><img src="<?php echo $countri->imagen; ?>"></a>
@@ -287,7 +287,7 @@ include_once("includes/funciones.php");
         <?php } ?>
       </div>
       <div class="d-md-block mt-5 text-center">
-        <a href="<?php echo mklink("propiedades/") ?>" class="btn btn-outline-secondary">ver todos <i class="fa fa-chevron-right ml-3"></i></a>
+        <a href="<?php echo mklink("propiedades/obras/") ?>" class="btn btn-outline-secondary">ver todos <i class="fa fa-chevron-right ml-3"></i></a>
       </div>
     </section>
   <?php } ?>
