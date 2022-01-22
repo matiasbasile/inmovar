e885cde7750c39c2a12c5201616a46e2133e1ce0
matiasbasile <basilematias99@gmail.com>
2022-01-22 16:46:39 -0300

    cambios

--- templates/grupourbano2/get_list.php
+++ templates/grupourbano2/get_list.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once("includes/init.php");
-$link_tipo_operacion = isset($_GET["tipo_operacion"]) ? intval($_GET["tipo_operacion"]) : "";
+$link_tipo_operacion = isset($_GET["tipo_operacion"]) ? $_GET["tipo_operacion"] : "";
 $id_localidad = isset($_GET["id_localidad"]) ? intval($_GET["id_localidad"]) : 0;
 $page = isset($_GET["page"]) ? intval($_GET["page"]) : 0;
 $offset = isset($_GET["offset"]) ? intval($_GET["offset"]) : 12;
