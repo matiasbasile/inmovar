0ee44566731d74eb70854a5e56e9c795978660c3
matiasbasile <basilematias99@gmail.com>
2022-01-22 17:13:06 -0300

    cambios

--- templates/grupourbano2/includes/propiedad/buscador.php
+++ templates/grupourbano2/includes/propiedad/buscador.php
@@ -1,41 +1,43 @@
 <a href="javascript:void(0)" rel="nofollow" class="btn btn-primary btn-block mb-3 form-toggle style-two mt-5">AJUSTAR BÚSQUEDA</a>
 <form onsubmit="return filtrar(this)" method="get" class="form-responsive mt-5">
   <div class="form-block">
-    <select class="form-control filter_propiedad" name="tp">
-      <option value="0">DEPARTAMENTOS</option>
-      <?php $tipo_propiedades = $propiedad_model->get_tipos_propiedades(); ?>
-      <?php foreach ($tipo_propiedades as $tipo) { ?>
-        <option <?php echo ($vc_id_tipo_inmueble == $tipo->id)?"selected":"" ?> value="<?php echo $tipo->id ?>"><?php echo $tipo->nombre ?></option>
-      <?php } ?>
-    </select>
-    <select class="form-control filter_localidad">
-      <option value="0">Localidad</option>
-      <?php $localidades = $propiedad_model->get_localidades(); ?>
-      <?php foreach ($localidades as $localidad) { ?>
-        <option <?php echo ($localidad->link == $vc_link_localidad)?"selected":"" ?> value="<?php echo $localidad->id ?>"><?php echo $localidad->nombre ?></option>
-      <?php } ?>
-    </select>
-    <select class="form-control filter_dormitorios" name="dm">
-      <option value="0">dormitorios</option>
-      <?php $dormitorios = $propiedad_model->get_dormitorios(); ?>
-      <?php foreach ($dormitorios as $dormitorio) { ?>
-        <option <?php echo ($vc_dormitorios == $dormitorio->dormitorios)?"selected":"" ?> value="<?php echo $dormitorio->dormitorios; ?>"><?php echo $dormitorio->dormitorios ?></option>
-      <?php } ?>
-    </select>
-    <select class="form-control filter_banios" name="bn">
-      <option value="0">baños</option>
-      <?php $banios = $propiedad_model->get_banios(); ?>
-      <?php foreach ($banios as $banio) { ?>
-        <option <?php echo ($vc_banios == $banio->banios)?"selected":"" ?> value="<?php echo $banio->banios; ?>"><?php echo $banio->banios ?></option>
-      <?php } ?>
-    </select>
-    <div class="inputs-with">
-      <input name="vc_minimo" class="form-control filter_minimo" type="number" value="<?php echo $vc_minimo ?>" min="0" placeholder="Precio Minimo">
+    <div class="form">
+      <select class="form-control filter_propiedad" name="tp">
+        <option value="0">DEPARTAMENTOS</option>
+        <?php $tipo_propiedades = $propiedad_model->get_tipos_propiedades(); ?>
+        <?php foreach ($tipo_propiedades as $tipo) { ?>
+          <option <?php echo ($vc_id_tipo_inmueble == $tipo->id)?"selected":"" ?> value="<?php echo $tipo->id ?>"><?php echo $tipo->nombre ?></option>
+        <?php } ?>
+      </select>
+      <select class="form-control filter_localidad">
+        <option value="0">Localidad</option>
+        <?php $localidades = $propiedad_model->get_localidades(); ?>
+        <?php foreach ($localidades as $localidad) { ?>
+          <option <?php echo ($localidad->link == $vc_link_localidad)?"selected":"" ?> value="<?php echo $localidad->id ?>"><?php echo $localidad->nombre ?></option>
+        <?php } ?>
+      </select>
+      <select class="form-control filter_dormitorios" name="dm">
+        <option value="0">dormitorios</option>
+        <?php $dormitorios = $propiedad_model->get_dormitorios(); ?>
+        <?php foreach ($dormitorios as $dormitorio) { ?>
+          <option <?php echo ($vc_dormitorios == $dormitorio->dormitorios)?"selected":"" ?> value="<?php echo $dormitorio->dormitorios; ?>"><?php echo $dormitorio->dormitorios ?></option>
+        <?php } ?>
+      </select>
+      <select class="form-control filter_banios" name="bn">
+        <option value="0">baños</option>
+        <?php $banios = $propiedad_model->get_banios(); ?>
+        <?php foreach ($banios as $banio) { ?>
+          <option <?php echo ($vc_banios == $banio->banios)?"selected":"" ?> value="<?php echo $banio->banios; ?>"><?php echo $banio->banios ?></option>
+        <?php } ?>
+      </select>
+      <div class="inputs-with">
+        <input name="vc_minimo" class="form-control filter_minimo" type="number" value="<?php echo $vc_minimo ?>" min="0" placeholder="Precio Minimo">
+      </div>
+      <div class="inputs-with">
+        <input name="vc_maximo" class="form-control filter_maximo" type="number" value="<?php echo $vc_maximo ?>" min="0" placeholder="Precio Maximo">
+      </div>
+      <button type="submit" class="btn btn-primary">BUSCAR</button>
     </div>
-    <div class="inputs-with">
-      <input name="vc_maximo" class="form-control filter_maximo" type="number" value="<?php echo $vc_maximo ?>" min="0" placeholder="Precio Maximo">
-    </div>
-    <button type="submit" class="btn btn-primary">BUSCAR</button>
   </div>
   <div class="page-heading mt-5">
     <div class="row justify-content-between">
