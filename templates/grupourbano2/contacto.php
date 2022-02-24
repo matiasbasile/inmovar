255178a297294cb544a590d7045ed4a9d6bab7d6
matiasbasile <basilematias99@gmail.com>
2022-02-23 21:57:40 -0300

    cambios

--- templates/grupourbano2/contacto.php
+++ templates/grupourbano2/contacto.php
@@ -31,13 +31,40 @@ $breadcrumb = array(
 
 <div class="container style-two">
   <div class="page-heading">
-    <h2>formulario de consulta</h2>
+    <h2>Información de Contacto</h2>
   </div>
   <div class="form">
     <div class="row">
-      <?php include("includes/form_contacto.php"); ?>
+      <div class="col-md-6">
+        <label class="label-control" id="contacto_nombre">Nombre *</label>
+        <input type="text" id="contacto_nombre" class="form-control" />
+      </div>
+      <div class="col-md-6">
+        <label class="label-control" id="contacto_apellido">Apellido *</label>
+        <input type="text" id="contacto_apellido" class="form-control" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <label class="label-control" id="contacto_email">Email *</label>
+        <input type="email" id="contacto_email" />
+      </div>
+      <div class="col-md-6">
+        <label class="label-control" id="contacto_telefono">Teléfono *</label>
+        <input type="tel" id="contacto_telefono" />
+      </div>
+    </div>
+    <div class="col-md-12">
+      <textarea id="contacto_mensaje" placeholder="Consulta"></textarea>
+    </div>
+    <div class="col-md-12">
+      <input type="submit" id="contacto_submit" value="enviar" class="btn btn-blue" />
     </div>
   </div>
+  <div class="page-heading">
+    <h2>Información de la Propiedad</h2>
+  </div>
+
 </div>
 <?php include("includes/footer.php"); ?>
 
@@ -52,6 +79,75 @@ $(".owl-carouselmarcas").owlCarousel({
       itemsMobile : [639,1],
     });
 });
+
+var enviando = 0;
+function enviar_contacto() {
+  if (enviando == 1) return;
+  var nombre = $("#contacto_nombre").val();
+  var email = $("#contacto_email").val();
+  var telefono = $("#contacto_telefono").val();
+  var mensaje = $("#contacto_mensaje").val();
+  var asunto = $("#contacto_asunto").val();
+  var para = $("#contacto_para").val();
+  var id_propiedad = $("#contacto_propiedad").val();
+  var id_usuario = $("#contacto_id_usuario").val();
+  if (isEmpty(para)) para = "<?php echo $empresa->email ?>";
+  
+  if (isEmpty(nombre) || nombre == "Nombre") {
+    alert("Por favor ingrese un nombre");
+    $("#contacto_nombre").focus();
+    return false;          
+  }
+  if (!validateEmail(email)) {
+    alert("Por favor ingrese un email valido");
+    $("#contacto_email").focus();
+    return false;          
+  }
+  if (isEmpty(telefono) || telefono == "Telefono") {
+    alert("Por favor ingrese un telefono");
+    $("#contacto_telefono").focus();
+    return false;          
+  }
+  if (isEmpty(mensaje) || mensaje == "Mensaje") {
+    alert("Por favor ingrese un mensaje");
+    $("#contacto_mensaje").focus();
+    return false;              
+  }    
+    
+  $("#contacto_submit").attr('disabled', 'disabled');
+  var datos = {
+    "nombre":nombre,
+    "email":email,
+    "mensaje":mensaje,
+    "telefono":telefono,
+    "asunto":asunto,
+    "para":para,
+    "id_propiedad":id_propiedad,
+    <?php if (isset($propiedad) && $propiedad->id_empresa != $empresa->id) { ?>
+      "id_empresa_relacion":"<?php echo $propiedad->id_empresa ?>",
+    <?php } ?>
+    "id_usuario":id_usuario,
+    "id_empresa":ID_EMPRESA,
+    "id_origen":<?php echo(isset($id_origen) ? $id_origen : 1); ?>,
+  }
+  enviando = 1;
+  $.ajax({
+    "url":"https://app.inmovar.com/admin/consultas/function/enviar/",
+    "type":"post",
+    "dataType":"json",
+    "data":datos,
+    "success":function(r){
+      if (r.error == 0) {
+        window.location.href = "<?php echo mklink ("web/gracias/") ?>";
+      } else {
+        alert("Ocurrio un error al enviar su email. Disculpe las molestias");
+        $("#contacto_submit").removeAttr('disabled');
+        enviando = 0;
+      }
+    }
+  });
+  return false;
+}
 </script>
 
 </body>
