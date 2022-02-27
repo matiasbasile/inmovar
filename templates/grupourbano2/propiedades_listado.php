4a1840a072249aa3b39f566c7ada532f739d3b56
matiasbasile <basilematias99@gmail.com>
2022-02-27 11:21:22 -0300

    cambios

--- templates/grupourbano2/propiedades_listado.php
+++ templates/grupourbano2/propiedades_listado.php
@@ -1,8 +1,8 @@
 <?php
 include_once("includes/init.php");
-$propiedades = extract($propiedad_model->get_variables(array(
-  "orden_default"=>8,
-)));
+if (!isset($config_grupo)) $config_grupo = array();
+$config_grupo["orden_default"] = 8;
+$propiedades = extract($propiedad_model->get_variables($config_grupo));
 if (isset($get_params["test"])) echo $propiedad_model->get_sql();
 $nombre_pagina = $vc_link_tipo_operacion;
 ?>
