09197277dde6d351d55a19c3e97e4d2e18b24c45
matiasbasile <basilematias99@gmail.com>
2022-03-31 12:20:35 -0300

    cartel en home de alaro

--- templates/alaro/includes_nuevo/head.php
+++ templates/alaro/includes_nuevo/head.php
@@ -4,5 +4,5 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css?v=1">
 <link rel="stylesheet" type="text/css" href="assets_nuevo/css/bootstrap.min.css?v=1" media="all">
 <link rel="stylesheet" type="text/css" href="assets_nuevo/css/swiper.min.css?v=1" media="all">
-<link href="assets_nuevo/css/style.css?v=1" type="text/css" media="all" rel="stylesheet" />
+<link href="assets_nuevo/css/style.css?v=3" type="text/css" media="all" rel="stylesheet" />
 <?php include ("templates/comun/post_head.php") ?>
