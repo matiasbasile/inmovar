bd641f79ef5ca6e8c412f8cc4120a00160858afd
matiasbasile <basilematias99@gmail.com>
2022-04-07 11:45:06 -0300

    cambios

--- templates/inmovar_homely/propiedades_detalle.php
+++ templates/inmovar_homely/propiedades_detalle.php
@@ -351,7 +351,7 @@ $propiedad_model->set_tracking_cookie(array("id_propiedad"=>$propiedad->id));
       </div><!-- end location -->
     <?php }?>
       <!-- end agent -->
-
+<?php exit(); ?>
       <div class="widget property-single-item property-location comment-form">
         <h4><span>Consultar por propiedad</span></h4><hr class="divisorline">
         <?php include("includes/form_contacto.php"); ?>
@@ -359,7 +359,7 @@ $propiedad_model->set_tracking_cookie(array("id_propiedad"=>$propiedad->id));
     </div><!-- end col -->
     
     <div class="col-lg-4 col-md-4 sidebar">
-      <?php exit(); ?>
+      
       <?php include "includes/sidebar.php" ?>
 
       <?php if (sizeof($propiedad->relacionados)>0) { ?>
