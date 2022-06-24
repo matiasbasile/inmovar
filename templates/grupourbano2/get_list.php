1173af61f8984402404bef05eafe4ea2dc1b8617
Matias Basile <basile.matias99@gmail.com>
2022-06-23 21:47:21 -0300

    cambios

--- templates/grupourbano2/get_list.php
+++ templates/grupourbano2/get_list.php
@@ -14,7 +14,8 @@ $es_oferta = isset($_GET["vc_es_oferta"]) ? intval($_GET["vc_es_oferta"]) : 0;
 $id_usuario = isset($_GET["id_usuario"]) ? intval($_GET["id_usuario"]) : 0;
 $order = isset($_GET["order"]) ? intval($_GET["order"]) : 8;
 $offset = isset($_GET["offset"]) ? intval($_GET["offset"]) : 12;
-extract($propiedad_model->get_variables(array(
+
+$config = array(
   "id_localidad"=>$id_localidad,
   "link_tipo_operacion"=>$link_tipo_operacion,
   "no_analizar_url"=>1,
@@ -27,7 +28,12 @@ extract($propiedad_model->get_variables(array(
   "in_ids_localidades"=>$vc_in_ids_localidades,
   "in_ids_tipo_inmueble"=>$vc_in_ids_tipo_inmueble,
   "in_dormitorios"=>$vc_in_dormitorios,
-)));
+);
+if ($es_oferta == 1) {
+  $config["solo_propias"] = 1;
+}
+
+extract($propiedad_model->get_variables($config));
 foreach ($vc_listado as $propiedad) { 
   item($propiedad,array(
     "clase"=>((!empty($id_usuario)) ? "col-md-6 p-0 neighborhoods-list" : "col-md-4 p-0 neighborhoods-list"),
