63b7fe3a182c37fe98a4897b8c7faa593b870ac9
Matias Basile <basile.matias99@gmail.com>
2022-08-24 11:01:44 -0300

    cambios

--- templates/alaro/includes_nuevo/head.php
+++ templates/alaro/includes_nuevo/head.php
@@ -4,6 +4,6 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css?v=1">
 <link rel="stylesheet" type="text/css" href="assets_nuevo/css/bootstrap.min.css?v=1" media="all">
 <link rel="stylesheet" type="text/css" href="assets_nuevo/css/swiper.min.css?v=1" media="all">
-<link href="assets_nuevo/css/style.css?v=11" type="text/css" media="all" rel="stylesheet" />
+<link href="assets_nuevo/css/style.css?v=12" type="text/css" media="all" rel="stylesheet" />
 <link href="css/cartel.css?v=1" type="text/css" media="all" rel="stylesheet" />
 <?php include ("templates/comun/post_head.php") ?>
