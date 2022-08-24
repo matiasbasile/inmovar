18ab5ad7ed6cc89a2a64cbdc192e77ff3cc1d5cb
Matias Basile <basile.matias99@gmail.com>
2022-08-24 09:07:27 -0300

    cambios

--- templates/grupourbano2/get_list.php
+++ templates/grupourbano2/get_list.php
@@ -10,7 +10,7 @@ $vc_in_ids_localidades = isset($_GET["vc_in_ids_localidades"]) ? ($_GET["vc_in_i
 $vc_in_ids_tipo_inmueble = isset($_GET["vc_in_ids_tipo_inmueble"]) ? ($_GET["vc_in_ids_tipo_inmueble"]) : "";
 $vc_in_dormitorios = isset($_GET["vc_in_dormitorios"]) ? ($_GET["vc_in_dormitorios"]) : "";
 $page = isset($_GET["page"]) ? intval($_GET["page"]) : 0;
-$es_oferta = isset($_GET["vc_es_oferta"]) ? intval($_GET["vc_es_oferta"]) : 0;
+$es_oferta = isset($_GET["vc_es_oferta"]) ? intval($_GET["vc_es_oferta"]) : -1;
 $solo_propias = isset($_GET["vc_solo_propias"]) ? intval($_GET["vc_solo_propias"]) : 0;
 $id_usuario = isset($_GET["id_usuario"]) ? intval($_GET["id_usuario"]) : 0;
 $order = isset($_GET["order"]) ? intval($_GET["order"]) : 8;
@@ -39,8 +39,5 @@ foreach ($vc_listado as $propiedad) {
   item($propiedad,array(
     "clase"=>((!empty($id_usuario)) ? "col-md-6 p-0 neighborhoods-list" : "col-md-4 p-0 neighborhoods-list"),
   ));
-}
-if ($id_usuario == 3579) {
-  echo $propiedad_model->get_sql();
 }
 ?>
