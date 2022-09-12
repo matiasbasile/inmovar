d6c654769160ce11ea356be5d483d7c9416ccaa5
manuelaprile <manuelaprile01@hotmail.com>
2022-09-12 09:07:30 -0300

    cambios

--- templates/francesconi/includes/home/novedades.php
+++ templates/francesconi/includes/home/novedades.php
@@ -4,7 +4,6 @@
   "order_by" => 1
 )); 
 ?>
-<?php print_r($novedades) ?>
 <?php if (!empty($novedades)) { ?>
   <section class="francesconi-noved">
     <div class="container">
@@ -19,19 +18,19 @@
               <div class="noved-warp">
                 <span><img src="assets/images/icons/icon-15.png" alt="Icon"></span>
                 <a href="#0" class="fill-btn">solidarias</a>
-                <img src="<?php echo $n->path ?>" alt="Noved">
+                <a href="<?php echo $n->link ?>">
+                  <img src="<?php echo $n->path ?>" alt="Noved">
+                </a>
               </div>
               <div class="noved-inner">
                 <h3><?php echo $n->titulo ?></h3>
-                <h5><?php echo $n->fecha ?></h5>
                 <?php 
                   $orderdate = explode('/', $n->fecha);
                   $month = $orderdate[1];
                   $day   = $orderdate[0];
                   $year  = $orderdate[2];
-                  echo $day . $month . $year;                  
                 ?>
-                <h5><small><?php echo $day ?></small>Junio del <?php echo $year ?></h5>
+                <h5><small><?php echo $day ?></small><?php echo date("F", strtotime($n->fecha_original)); ?> del <?php echo $year ?></h5>
                 <p>
                   <?php echo $n->plain_text ?></p>
               </div>
