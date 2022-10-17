db7f1aef76cac4893349c2f4c312c2b304cb185e
manuelaprile <manuelaprile01@hotmail.com>
2022-10-17 13:10:25 -0300

    cambios

--- templates/inmovar_1/includes/form_contacto.php
+++ templates/inmovar_1/includes/form_contacto.php
@@ -47,10 +47,10 @@
           <?php include 'prefijo_localidades.php' ?>
         </div>
         <div class="chat_user_form_row_6">
-          <input type="text" id="contacto_telefono" value="<?php echo isset($_SESSION["telefono"]) ? $_SESSION["telefono"] : "" ?>"" class="chat_user_form_input chat_user_form_2_celular" placeholder="Teléfono">
+          <input type="text" id="chat_user_form_2_celular" value="<?php echo isset($_SESSION["telefono"]) ? $_SESSION["telefono"] : "" ?>"" class="chat_user_form_input chat_user_form_2_celular" placeholder="Teléfono">
         </div>
       </div>
-        <input class="form-control" type="tel" id="contacto_telefono" value="<?php echo isset($_SESSION["telefono"]) ? $_SESSION["telefono"] : "" ?>" placeholder="Tel&eacute;fono" />
+        <!-- <input class="form-control" type="tel" id="contacto_telefono" value="<?php echo isset($_SESSION["telefono"]) ? $_SESSION["telefono"] : "" ?>" placeholder="Tel&eacute;fono" /> -->
       </div>
       </div>
       <div class="col-md-12">
