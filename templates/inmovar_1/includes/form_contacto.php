250d412e2e78b225f4fd9c897884035f4d760a40
manuelaprile <manuelaprile01@hotmail.com>
2022-10-18 08:23:57 -0300

    Agregado prefijo y select en inmovar homely

--- templates/inmovar_1/includes/form_contacto.php
+++ templates/inmovar_1/includes/form_contacto.php
@@ -47,7 +47,7 @@
               <?php include 'prefijo_localidades.php' ?>
             </div>
             <div class="chat_user_form_row_6">
-              <input type="text" id="contacto_telefono" value="<?php echo isset($_SESSION["telefono"]) ? $_SESSION["telefono"] : "" ?>"" class=" chat_user_form_input chat_user_form_2_celular" placeholder="Teléfono">
+              <input type="text" id="contacto_telefono" value="<?php echo isset($_SESSION["telefono"]) ? $_SESSION["telefono"] : "" ?>"" class="chat_user_form_input chat_user_form_2_celular" placeholder="Teléfono">
             </div>
           </div>
           <!-- <input class="form-control" type="tel" id="contacto_telefono" value="<?php echo isset($_SESSION["telefono"]) ? $_SESSION["telefono"] : "" ?>" placeholder="Tel&eacute;fono" /> -->
