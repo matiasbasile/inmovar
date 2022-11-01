d3b77732677489d8d3e7b79f7610335d7f22dc8c
Matias Basile <basile.matias99@gmail.com>
2022-10-31 22:59:34 -0300

    cambios

--- templates/inmovar_1/home.php
+++ templates/inmovar_1/home.php
@@ -35,7 +35,19 @@ $tipo_operacion->id = 0;
 
   <?php include("includes/home/slider.php"); ?>
 
-  <?php include("includes/home/search.php"); ?>
+  <div class="search-box-wrapper search-box-home">
+    <div class="search-box-inner">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-md-offset-9 col-sm-4 col-sm-offset-8">
+            <div class="search-box map">
+              <?php include("includes/buscador.php"); ?>
+            </div><!-- /.search-box.map -->
+          </div><!-- /.col-md-3 -->
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </div><!-- /.search-box-inner -->
+  </div>
 
   <div id="page-content">
 
