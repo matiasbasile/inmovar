a52cc4ec7eb87adcec9bbee8332803c078e7fc45
Matias Basile <basile.matias99@gmail.com>
2022-11-01 10:59:58 -0300

    cambios

--- templates/francesconi/noticias_listado.php
+++ templates/francesconi/noticias_listado.php
@@ -43,7 +43,7 @@ extract($entrada_model->get_variables(array(
                 <label for="" style="font-weight: bold;">FILTRAR POR CATEGORÍA:</label>
                 <div class="select-inner">
                   <select class="round" name="cat" id="categoria">
-                    <?php $categorias = $entrada_model->get_subcategorias(0,array("not_in"=>"1679,1680")) ?>
+                    <?php $categorias = $entrada_model->get_subcategorias(1681) ?>
                     <?php foreach ($categorias as $cat) { ?>
                       <option <?php echo ($cat->id == $vc_id_categoria) ? "selected" : "" ?> value="<?php echo $cat->id ?>"><?php echo strtolower($cat->nombre) ?></option>
                     <?php } ?>
