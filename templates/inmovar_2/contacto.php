c2909d789cc283b3602bdd9399be1227c94edeb6
Matias Basile <basile.matias99@gmail.com>
2022-11-08 23:40:31 -0300

    cambios

--- templates/inmovar_2/contacto.php
+++ templates/inmovar_2/contacto.php
@@ -83,12 +83,30 @@ $titulo_pagina = "Contacto"; ?>
                 <div class="media-body">
                   <h4>Dirección</h4>
                   <p><?php echo $empresa->direccion ?></p>
-                  <?php if (!empty($empresa->direccion_2)) { ?>
-                    <p><?php echo $empresa->direccion_2 ?></p>
-                  <?php } ?>
-                  <?php if (!empty($empresa->direccion_3)) { ?>
-                    <p><?php echo $empresa->direccion_3 ?></p>
-                  <?php } ?>
+                </div>
+              </div>
+            <?php } ?>
+
+            <?php if (!empty($empresa->direccion_2)) {  ?>
+              <div class="media">
+                <div class="media-left">
+                  <i class="fa fa-map-marker"></i>
+                </div>
+                <div class="media-body">
+                  <h4>Dirección</h4>
+                  <p><?php echo $empresa->direccion_2 ?></p>
+                </div>
+              </div>
+            <?php } ?>
+
+            <?php if (!empty($empresa->direccion_3)) {  ?>
+              <div class="media">
+                <div class="media-left">
+                  <i class="fa fa-map-marker"></i>
+                </div>
+                <div class="media-body">
+                  <h4>Dirección</h4>
+                  <p><?php echo $empresa->direccion_3 ?></p>
                 </div>
               </div>
             <?php } ?>
