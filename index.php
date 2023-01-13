b4ebb5bb4347771bbfd55a7ee0cbd62db5a46150
Matias Basile <matias.basile@varcreative.com>
2023-01-13 08:27:46 -0300

    index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('display_errors', 1);
+ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 if ( extension_loaded( 'zlib' ) ) { ob_start( 'ob_gzhandler' ); }
