14684bff71f2b94972a71f3a72c669680fe508b6
Matias Basile <matias.basile@varcreative.com>
2023-01-17 07:07:16 -0300

    Cambios

--- admin/application/controllers/app.php
+++ admin/application/controllers/app.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class App extends CI_Controller {
+class App extends CI_Controller { 
 
   function __construct() {
     parent::__construct();
