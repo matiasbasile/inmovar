769b02186e61ba520183f71db71d09ff7f0f5ad8
Matias Basile <matias.basile@varcreative.com>
2023-02-11 17:25:48 -0300

    Cambios

--- templates/francesconi/propiedades_listado.php
+++ templates/francesconi/propiedades_listado.php
@@ -57,6 +57,7 @@ $nombre_pagina = $vc_link_tipo_operacion;
           </div>
         </div>
       </div>
+      <?php echo $vc_total_paginas ?>
       <div class="text-center <?php echo ($vc_total_paginas == 1) ? 'd-none' : '' ?>">
         <a onclick="cargar()" id="cargarMas" class="fill-btn">ver más propiedades</a>
       </div>
