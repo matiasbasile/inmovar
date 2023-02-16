326c8fa8a2c421098804d6b62ea143947069d96d
Matias Basile <matias.basile@varcreative.com>
2023-02-16 14:50:33 -0300

    Cambios

--- templates/francesconi/includes/head.php
+++ templates/francesconi/includes/head.php
@@ -5,7 +5,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Antonio:wght@400;500;600;700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Arapey:ital@0;1&display=swap" rel="stylesheet">
 <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
-<link rel="stylesheet" type="text/css" href="assets/css/global.css?v=11">
+<link rel="stylesheet" type="text/css" href="assets/css/global.css?v=12">
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <?php include "templates/comun/post_head.php" ?>
