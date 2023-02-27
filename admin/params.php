3e09bd0d03ad9d367da8158a5a2a12322fdb34fb
Matias Basile <matias.basile@varcreative.com>
2023-02-27 11:57:41 -0300

    agregue config

--- admin/params.php
+++ admin/params.php
@@ -6,7 +6,7 @@ date_default_timezone_set("America/Argentina/Buenos_Aires");
 if (!defined("SERVER_DB")) { DEFINE ("SERVER_DB","localhost"); }
 if (!defined("DATABASE")) { DEFINE ("DATABASE","inmovar"); }
 if (!defined("USER_DB")) { DEFINE ("USER_DB","root"); }
-if (!defined("PASSWORD_DB")) { DEFINE ("PASSWORD_DB",""); }
+if (!defined("PASSWORD_DB")) { DEFINE ("PASSWORD_DB","varcreative1805"); }
 if (!defined("FORCE_HTTPS")) { DEFINE ("FORCE_HTTPS",false); }
 if ( !defined("DOMINIO")) { DEFINE ("DOMINIO","http://app.inmovar.com/"); }
 
