8a2cc3855f0fcd455f05b6b3fb527aa5a575de7d
Matias Basile <matias.basile@varcreative.com>
2023-06-23 09:25:50 -0300

    cambios en alaro

--- templates/alaro/includes_nuevo/head_new.php
+++ templates/alaro/includes_nuevo/head_new.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css?v=1">
 <link href="css/correa/bootstrap.min.css?v=1" rel="stylesheet" type="text/css" media="all">
 <link href="css/correa/slick-theme.css?v=1" rel="stylesheet" type="text/css">
-<link href="css/correa/styles.css?v=1" rel="stylesheet" type="text/css" media="all">
+<link href="css/correa/styles.css?v=2" rel="stylesheet" type="text/css" media="all">
 <link href="css/correa/responsive.css?v=1" rel="stylesheet" type="text/css" media="all">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
