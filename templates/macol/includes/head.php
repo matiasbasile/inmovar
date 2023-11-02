66d7c704f42173727784d7b679e6be1419ef5d8f
Matias Basile <matias.basile@varcreative.com>
2023-11-02 10:03:22 -0300

    Cambios

--- templates/macol/includes/head.php
+++ templates/macol/includes/head.php
@@ -8,5 +8,6 @@
     href="https://fonts.googleapis.com/css2?family=Lato&family=Poppins:wght@400;700&family=Roboto:wght@300;400;500;700;900&display=swap"
     rel="stylesheet">
 <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" href="assets/css/fancybox.css">
 <link rel="stylesheet" type="text/css" href="assets/css/global.css?v=3">
 <?php include 'templates/comun/post_head.php'; ?>
