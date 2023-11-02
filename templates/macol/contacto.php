3036e706595c7273fd6c1feb216c03ddd2412c5c
Matias Basile <matias.basile@varcreative.com>
2023-11-02 10:14:28 -0300

    Cambios

--- templates/macol/contacto.php
+++ templates/macol/contacto.php
@@ -26,154 +26,42 @@ include 'includes/head.php'; ?>
     </section>
     <?php } ?>
 
-    <!-- Communicate Us Two -->
-    <section class="communicate-us-two">
-        <div class="container">
-            <div class="section-title">
-                <?php $titulo = $web_model->get_text('contacto-titulo', 'Comunicate con nosotros'); ?>
-                <h2 class="editable" data-id="<?php echo $titulo->id; ?>" data-clave="<?php echo $titulo->clave; ?>">
-                    <?php echo $titulo->plain_text; ?>
-                </h2>
-                <?php $descripcion = $web_model->get_text('contacto-descripcion', 'Descripcion'); ?>
-                <p class="editable" data-id="<?php echo $descripcion->id; ?>"
-                    data-clave="<?php echo $descripcion->clave; ?>">
-                    <?php echo $descripcion->plain_text; ?>
-                </p>
-            </div>
-            <div class="contact-wrap">
-                <div class="row">
-                    <div class="col-md-4 col-6">
-                        <div class="communicate-items">
-                            <img src="assets/images/map-logo.png" alt="Location">
-                            <?php $direccion = $web_model->get_text('direccion', 'DIRECCIÓN'); ?>
-                            <h3 class="editable" data-id="<?php echo $direccion->id; ?>"
-                                data-clave="<?php echo $direccion->clave; ?>">
-                                <?php echo $direccion->plain_text; ?>
-                            </h3>
-
-                            <p><?php echo $empresa->direccion; ?></p>
-                        </div>
-                    </div>
-                    <div class="col-md-4 col-6">
-                        <div class="communicate-items">
-                            <img src="assets/images/calendar.png" alt="Calendar">
-                            <?php $horarios = $web_model->get_text('horarios', 'HORARIOS'); ?>
-                            <h3 class="editable" data-id="<?php echo $horarios->id; ?>"
-                                data-clave="<?php echo $horarios->clave; ?>">
-                                <?php echo $horarios->plain_text; ?>
-                            </h3>
-                            <p><?php echo $empresa->horario; ?></p>
-                        </div>
-                    </div>
-                    <div class="col-md-4 col-6">
-                        <div class="communicate-items">
-                            <img src="assets/images/phone.png" alt="Phone">
-                            <?php $contacto = $web_model->get_text('contacto', 'CONTACTO'); ?>
-                            <h3 class="editable" data-id="<?php echo $contacto->id; ?>"
-                                data-clave="<?php echo $contacto->clave; ?>">
-                                <?php echo $contacto->plain_text; ?>
-                            </h3>
-                            <p class="d-flex flex-column">
-                                <a href="tel:<?php echo $empresa->telefono; ?>"><?php echo $empresa->telefono; ?></a>
-                                <a href="mailto:<?php echo $empresa->email; ?>"><?php echo $empresa->email; ?></a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="form-wrap form-wrap-two form-wrap-three">
-                <div class="section-title">
-                    <?php $consulta = $web_model->get_text('consulta', 'Envía una consulta'); ?>
-                    <h2 class="editable" data-id="<?php echo $consulta->id; ?>"
-                        data-clave="<?php echo $consulta->clave; ?>">
-                        <?php echo $consulta->plain_text; ?>
-                    </h2>
-                </div>
-                <form id="contactForm">
-                    <div class="row">
-                        <div class="col-6">
-                            <div class="form-group">
-                                <input id="contacto_nombre" class="form-control" type="text" placeholder="Nombre">
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="form-group">
-                                <input id="contacto_email" class="form-control" type="email" placeholder="Email">
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="form-group">
-                                <input id="contacto_telefono" class="form-control" type="text"
-                                    placeholder="Whatsapp (Cod. área sin 0 ni 15)">
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="form-group">
-                                <select name="" id="contacto_asunto" class="form-control">
-                                    <option value="">Asunto</option>
-                                    <?php $asuntos = explode(';;;', $empresa->asuntos_contacto); ?>
-                                    <?php foreach ($asuntos as $a) { ?>
-                                    <option><?php echo $a; ?></option>
-                                    <?php } ?>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="form-group">
-                                <textarea id="contacto_mensaje" placeholder="Mensaje" class="form-control"></textarea>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="btn-block">
-                        <button type="submit" id="contacto_submit" class="btn">enviar consulta</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </section>
+    <?php include 'includes/contacto.php'; ?>
 
     <!-- Footer -->
     <?php include 'includes/footer.php'; ?>
 
-    <!-- Scripts -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/script.js"></script>
     <?php
-
     if (isset($empresa->latitud) && isset($empresa->longitud)) {
-        include_once 'templates/comun/mapa_js.php';
+        include_once 'templates/comun/mapa_js.php'; ?>
+        <script>
+        $(document).ready(function() {
+            /* if ($("#map").length == 0) return; */
+            var mymap = L.map('map').setView([<?php echo $empresa->latitud; ?>, <?php echo $empresa->longitud; ?>],
+                16);
 
-        ?>
-    <script>
-    $(document).ready(function() {
-        /* if ($("#map").length == 0) return; */
-        var mymap = L.map('map').setView([<?php echo $empresa->latitud; ?>, <?php echo $empresa->longitud; ?>],
-            16);
+            L.tileLayer(
+                'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=<?php echo defined('MAPBOX_KEY') ? MAPBOX_KEY : ''; ?>', {
+                    attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+                    tileSize: 512,
+                    maxZoom: 18,
+                    zoomOffset: -1,
+                    id: 'mapbox/streets-v11',
+                    accessToken: '<?php echo defined('MAPBOX_KEY') ? MAPBOX_KEY : ''; ?>',
+                }).addTo(mymap);
 
-        L.tileLayer(
-            'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=<?php echo defined('MAPBOX_KEY') ? MAPBOX_KEY : ''; ?>', {
-                attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
-                tileSize: 512,
-                maxZoom: 18,
-                zoomOffset: -1,
-                id: 'mapbox/streets-v11',
-                accessToken: '<?php echo defined('MAPBOX_KEY') ? MAPBOX_KEY : ''; ?>',
+
+            var icono = L.icon({
+                iconUrl: 'assets/images/map-place.png',
+                iconSize: [60, 60], // size of the icon
+                iconAnchor: [30, 30], // point of the icon which will correspond to marker's location
+            });
+
+            L.marker([<?php echo $empresa->latitud; ?>, <?php echo $empresa->longitud; ?>], {
+                icon: icono
             }).addTo(mymap);
-
-
-        var icono = L.icon({
-            iconUrl: 'assets/images/map-place.png',
-            iconSize: [60, 60], // size of the icon
-            iconAnchor: [30, 30], // point of the icon which will correspond to marker's location
         });
-
-        L.marker([<?php echo $empresa->latitud; ?>, <?php echo $empresa->longitud; ?>], {
-            icon: icono
-        }).addTo(mymap);
-    });
-    </script>
+        </script>
     <?php } ?>
 </body>
 
--- templates/macol/noticias_detalle.php
+++ templates/macol/noticias_detalle.php
@@ -26,62 +26,10 @@ $entrada = $entrada_model->get($id);
     </div>
   </section>
 
-  <section class="communicate-us" style="padding-top:40px;">
-    <div class="row gy-5 gx-0 align-items-center">
-      <div class="form-wrap form-wrap-two">
-        <form id="contactForm">
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="form-group">
-                <input id="contacto_nombre" class="form-control" type="text"
-                  placeholder="Nombre completo">
-              </div>
-            </div>
-            <div class="col-sm-6">
-              <div class="form-group">
-                <input id="contacto_telefono" class="form-control" type="text"
-                  placeholder="Whatsapp (sin 0 ni 15)">
-              </div>
-            </div>
-            <div class="col-sm-6">
-              <div class="form-group">
-                <input id="contacto_email" class="form-control" type="email" placeholder="Email">
-              </div>
-            </div>
-            <div class="col-sm-6">
-              <div class="form-group">
-                <select id="contacto_asunto" class="form-control">
-                  <option value="">Elija asunto</option>
-                  <?php $asuntos = explode(';;;', $empresa->asuntos_contacto); ?>
-                  <?php foreach ($asuntos as $a) { ?>
-                  <option><?php echo $a; ?></option>
-                  <?php } ?>
-                </select>
-              </div>
-            </div>
-            <div class="col-12">
-              <div class="form-group">
-                <textarea id="contacto_mensaje" placeholder="Escriba sus comentarios"
-                  class="form-control"></textarea>
-              </div>
-            </div>
-          </div>
-          <button id="contacto_submit" class="btn">enviar consulta</button>
-        </form>
-      </div>
-    </div>
-  </section>
+  <?php include 'includes/contacto.php'; ?>
 
-  <!-- Footer -->
   <?php include 'includes/footer.php'; ?>
 
-  <!-- Scripts -->
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/bootstrap.bundle.min.js"></script>
-  <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWmUapFYTBXV3IJL9ggjT9Z1wppCER55g&callback=initMap">
-  </script>
-  <script src="assets/js/script.js"></script>
 </body>
 
 </html>
