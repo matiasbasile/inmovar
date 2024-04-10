bd623c67f93e2b011183a224ea52eafdc2f3fc09
mbasile <m.basile@avoristravel.com>
2024-04-10 14:58:48 -0300

    fix: open graph

--- templates/comun/og.php
+++ templates/comun/og.php
@@ -1,8 +1,8 @@
 <meta property="og:url" content="<?php echo current_url(); ?>" />
 <meta property="og:type" content="website" />
-<meta property="og:description" content="<?php echo $propiedad->seo_description; ?>" />
-<meta property="og:site_name" content="<?php echo $empresa->nombre ?>">
-<meta property="og:title" content="<?php echo $propiedad->seo_title ?>" />
+<meta property="og:description" content="<?php echo trim($propiedad->seo_description); ?>" />
+<meta property="og:site_name" content="<?php echo trim($empresa->nombre) ?>">
+<meta property="og:title" content="<?php echo trim($propiedad->seo_title) ?>" />
 
 <?php
 $imagen_ppal = "";
