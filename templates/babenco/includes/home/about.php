ef43bff4f1708cafabc8f3a55de1f2d0e54504f4
mbasile <m.basile@avoristravel.com>
2024-06-21 11:09:22 -0300

    babenco

--- templates/babenco/includes/home/about.php
+++ templates/babenco/includes/home/about.php
@@ -22,15 +22,15 @@
             <?php echo $t->plain_text ?>
           </span>
           <?php $t = $web_model->get_text("about-title-1","Experiencia Inmobiliaria <br>Confiable desde 1985"); ?>
-          <div class="editable" data-id_empresa="<?php echo $t->id_empresa ?>" data-clave="<?php echo $t->clave ?>" data-id="<?php echo $t->id ?>">
+          <h2 class="editable" data-id_empresa="<?php echo $t->id_empresa ?>" data-clave="<?php echo $t->clave ?>" data-id="<?php echo $t->id ?>">
             <?php echo $t->plain_text ?>
-          </div>
+          </h2>
         </div>
 
         <?php $t = $web_model->get_text("about-text-1","<p>Nuestra trayectoria de décadas habla por sí misma. Desde 1985, hemos sido el pilar de confianza en el mercado inmobiliario, ofreciendo un servicio excepcional y resultados probados. Nuestro compromiso es hacer realidad tus sueños de hogar.</p>"); ?>
-        <div class="editable" data-id_empresa="<?php echo $t->id_empresa ?>" data-clave="<?php echo $t->clave ?>" data-id="<?php echo $t->id ?>">
-          <?php echo $t->texto ?>
-        </div>
+        <p class="editable" data-id_empresa="<?php echo $t->id_empresa ?>" data-clave="<?php echo $t->clave ?>" data-id="<?php echo $t->id ?>">
+          <?php echo $t->plain_text ?>
+        </p>
         
         <div class="row">
           <div class="col-md-7">
