51d0e7aee844e54986a7429d93e126597d85ffb1
mbasile <m.basile@avoristravel.com>
2024-06-21 10:56:33 -0300

    babenco

--- templates/babenco/includes/home/oficinas.php
+++ templates/babenco/includes/home/oficinas.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-md-6">
         <?php $t = $web_model->get_text("home-sucursal-1-img","assets/images/property1.png"); ?>
-        <div class="img-box editable" data-id_empresa="<?php echo $t->id_empresa ?>" data-clave="<?php echo $t->clave ?>" data-id="<?php echo $t->id ?>">
+        <div class="img-box editable editable-img" data-id_empresa="<?php echo $t->id_empresa ?>" data-clave="<?php echo $t->clave ?>" data-id="<?php echo $t->id ?>">
           <img src="<?php echo $t->path ?>" alt="Property Img">
           <div class="overley-box">
             <h2><?php echo $t->plain_text ?></h2>
