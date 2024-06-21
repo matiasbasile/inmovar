e1fa45ee1365fe323a12536ba0671da7d669469e
mbasile <m.basile@avoristravel.com>
2024-06-21 09:15:21 -0300

    babenco

--- templates/babenco/includes/init.php
+++ templates/babenco/includes/init.php
@@ -1,4 +1,8 @@
-<?php 
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include  "models/Web_Model.php";
 $web_model = new Web_Model ($empresa->id,$conx); 
 include  "models/Entrada_Model.php";
