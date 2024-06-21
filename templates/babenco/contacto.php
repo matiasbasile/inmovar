4eec59caf61e7faddb70549a314ec8e0e3b38f9b
mbasile <m.basile@avoristravel.com>
2024-06-21 10:41:24 -0300

    babenco

---
+++ templates/babenco/contacto.php
@@ -0,0 +1,21 @@
+<?php include 'includes/init.php' ?>
+<!DOCTYPE html>
+<html lang="es" >
+<head>
+  <?php include("includes/head.php") ?>
+</head>
+<body>
+  <?php include("includes/header.php") ?>
+
+<section class="page-title">
+  <div class="container">
+    <h1>contacto</h1>
+  </div>
+</section>
+
+<?php include("includes/contacto.php") ?>
+
+<?php include("includes/footer.php") ?>
+
+</body>
+</html>
