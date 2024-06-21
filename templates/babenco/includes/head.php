16ec26cf25cf2d3b1433a30003ece431bd23b680
mbasile <m.basile@avoristravel.com>
2024-06-21 10:22:25 -0300

    babenco

--- templates/babenco/includes/head.php
+++ templates/babenco/includes/head.php
@@ -3,7 +3,10 @@
 <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
 <link rel="stylesheet" href="assets/css/font-awesome.min.css">
 <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/fancybox.css">
 <link rel="stylesheet" href="assets/css/style.css?v=1">
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
 <?php include "templates/comun/post_head.php" ?>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="assets/css/jquery-ui.min.css">
