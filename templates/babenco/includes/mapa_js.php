effdc8a23f98ce12b3882d0744cbb7b06aaf9a4a
mbasile <m.basile@avoristravel.com>
2024-06-25 10:43:20 -0300

    babenco

---
+++ templates/babenco/includes/mapa_js.php
@@ -0,0 +1,52 @@
+<script type="text/javascript">
+$(document).ready(function() {
+
+  var mymap = L.map('mapa').setView([<?php echo $empresa->latitud ?>, <?php echo $empresa->longitud ?>], 15);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=<?php echo (defined("MAPBOX_KEY") ? MAPBOX_KEY : "") ?>', {
+    attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: 'mapbox/streets-v11',
+    accessToken: '<?php echo (defined("MAPBOX_KEY") ? MAPBOX_KEY : "") ?>',
+  }).addTo(mymap);
+
+
+  mymap.fitBounds([
+    <?php foreach ($vc_listado as $p) {
+      if (isset($p->latitud) && isset($p->longitud) && !empty($p->latitud) && !empty($p->longitud)) {  ?>[<?php echo $p->latitud ?>, <?php echo $p->longitud ?>],
+      <?php } ?>
+    <?php } ?>
+  ]);
+
+  /*var icono = L.icon({
+    iconUrl: 'images/map-place.png',
+    iconSize: [60, 60], // size of the icon
+    iconAnchor: [30, 30], // point of the icon which will correspond to marker's location
+  });*/
+
+  <?php $i = 0;
+  foreach ($vc_listado as $p) {
+    if (isset($p->latitud) && isset($p->longitud) && !empty($p->latitud) && !empty($p->longitud)) { ?>
+      var contentString<?php echo $i; ?> = '<div id="content">' +
+        '<div style="padding: 0px;">' +
+        <?php if (!empty($p->link_propiedad)) { ?> '<a href=\"<?php echo $p->link_propiedad ?>\">' + <?php } ?> '<h4 style="font-size:20px;margin:5px 0px"><?php echo ($p->nombre) ?></h4>' +
+        '<p style="font-size:16px;color:#222;margin:0px;"><?php echo ($p->direccion_completa . " | " . $p->localidad) ?></p>' +
+        <?php if (!empty($p->link_propiedad)) { ?> '</a>' + <?php } ?>
+      <?php if (!empty($p->link_propiedad)) { ?> '<a href=\"<?php echo $p->link_propiedad ?>\">' + <?php } ?> '<img width=\"200\" src=\"<?php echo $p->imagen ?>\"/>' +
+        <?php if (!empty($p->link_propiedad)) { ?> '</a>' + <?php } ?> '</div>' +
+        '</div>';
+
+        var marker<?php echo $i; ?> = L.marker([<?php echo $p->latitud ?>, <?php echo $p->longitud ?>], {
+          //icon: icono
+        });
+        marker<?php echo $i; ?>.addTo(mymap);
+
+        marker<?php echo $i; ?>.bindPopup(contentString<?php echo $i; ?>);
+
+      <?php } ?>
+    <?php $i++;
+  } ?>
+
+});
+</script>
---
+++ templates/babenco/mapa.php
@@ -0,0 +1,80 @@
+<?php 
+include 'includes/init.php';
+
+if (!isset($config_grupo)) $config_grupo = array();
+$config_grupo["orden_default"] = 8;
+$config_grupo["images_limit"] = 3;
+
+// Si tiene el flag de ofertas
+if (isset($buscar_ofertas)) {
+  $config_grupo["solo_propias"] = 1;
+  $config_grupo["es_oferta"] = 1;
+}
+
+extract($propiedad_model->get_variables($config_grupo));
+if (isset($get_params["test"])) echo $propiedad_model->get_sql();
+$nombre_pagina = $vc_link_tipo_operacion;
+
+if (isset($get_params["tp"]) && ($get_params["tp"] == "27" || $get_params["tp"] == "4")) $vc_nombre_operacion = "Barrios Cerrados";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <?php include("includes/head.php") ?>
+</head>
+<body>
+
+  <?php include("includes/header.php") ?>
+
+<section class="page-title">
+  <div class="container">
+    <h1><?php 
+      echo ($empresa->id == ID_EMPRESA_LA_PLATA) ? "La Plata" : "Punta del Este";
+    ?></h1>
+  </div>
+</section>
+
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <?php include("includes/propiedad/buscador.php") ?>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Properties Details  -->
+<section class="properties-details">
+  <div class="container">
+    <div class="properties">
+      <div class="sort">
+        <div class="inner-text">
+          <h4>Propiedades en <?php echo $vc_nombre_operacion ?></h4>
+          <p>Se encontraron <strong><?php echo $vc_total_resultados ?></strong> propiedades</p>
+        </div>
+        <div class="right-text">
+          <p>Ordenar por:</p>
+          <select onchange="ordenar()" id="orden" class="form-select form-control">
+            <option value="8" <?php echo ($vc_orden == 8) ? "selected" : "" ?>>Propiedades Destacadas</option>
+            <option value="2" <?php echo ($vc_orden == 2) ? "selected" : "" ?>>precio de menor a mayor</option>
+            <option value="1" <?php echo ($vc_orden == 1) ? "selected" : "" ?>>precio de mayor a menor</option>
+          </select>
+          <div class="location">
+            <a href="javascript:void(0)" onclick="buscar_mapa()" class="green"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+              <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+            </svg>Mapa</a>
+          </div>
+        </div>
+      </div>
+
+      <div id="mapa" style="width:100%; height:700px"></div>
+      
+    </div>      
+  </div>
+</section>
+
+<?php include("includes/footer.php") ?>
+<?php include("includes/mapa_js.php"); ?>
+</body>
+</html>
