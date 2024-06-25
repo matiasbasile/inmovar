2b300b1ee02fece1aa3a11fc84abd4ec277b85af
mbasile <m.basile@avoristravel.com>
2024-06-25 10:28:37 -0300

    babenco

--- templates/babenco/nosotros.php
+++ templates/babenco/nosotros.php
@@ -40,40 +40,5 @@
 
 <?php include("includes/footer.php") ?>
 
-<?php if (sizeof($oficinas_list)>0) { ?>
-  <script type="text/javascript">
-    $(document).ready(function(){
-
-      var icono = L.icon({
-        iconUrl: 'assets/images/map-icon.png',
-      });
-
-      <?php
-      $i = 0; 
-      foreach($oficinas_list as $oficina) {
-        if (empty($oficina->link_externo)) continue;
-        $coordenadas = explode(",", $oficina->link_externo);
-        if (sizeof($coordenadas) != 2) continue;
-        $oficina->latitud = trim($coordenadas[0]);
-        $oficina->longitud = trim($coordenadas[1]);
-        ?>
-        var mymap = L.map('map<?php echo $i ?>').setView([<?php echo $oficina->latitud ?>,<?php echo $oficina->longitud ?>], 15);
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidmNtYXBib3gxNCIsImEiOiJjbHVzZGpndTAwMXBmMnZwZHVzaHpwdnBkIn0.MNg411Qzoi2JfvKRn6qe2A', {
-          attribution: '',
-          tileSize: 512,
-          maxZoom: 18,
-          zoomOffset: -1,
-          id: 'mapbox/streets-v11',
-          accessToken: 'pk.eyJ1IjoidmNtYXBib3gxNCIsImEiOiJjbHVzZGpndTAwMXBmMnZwZHVzaHpwdnBkIn0.MNg411Qzoi2JfvKRn6qe2A',
-        }).addTo(mymap);
-
-        L.marker([<?php echo $oficina->latitud ?>,<?php echo $oficina->longitud ?>],{
-          icon: icono
-        }).addTo(mymap);
-      <?php $i++; } ?>
-
-    });
-  </script>
-<?php } ?>
 </body>
 </html>
