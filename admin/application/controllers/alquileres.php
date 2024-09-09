9386aaf33792215badb87e98ae790b9e41cd92f8
mbasile <m.basile@avoristravel.com>
2024-09-09 08:53:57 -0300

    fix: recibo de alquileres para propietario

--- admin/application/controllers/alquileres.php
+++ admin/application/controllers/alquileres.php
@@ -533,7 +533,7 @@ class Alquileres extends REST_Controller {
     $sql.= " DATE_FORMAT(AC.vencimiento,'%d/%m/%Y') AS vencimiento, AC.monto, AC.expensa, (AC.monto+AC.expensa) AS total, ";
     $sql.= " P.nombre AS propiedad, CONCAT(P.calle,' ',P.altura,' ',P.piso,' ',P.numero) AS direccion, ";
     $sql.= " IF(A.moneda = '', '$', A.moneda) as moneda, ";
-    $sql.= " IF(CP.nombre IS NULL,'',CONCAT(CP.nombre,' ',CP.apellido)) AS propietario, ";
+    $sql.= " IF(CP.nombre IS NULL,'',CP.nombre) AS propietario, ";
     $sql.= " IF(CP.cuit IS NULL,'',CP.cuit) AS propietario_dni ";
     $sql.= "FROM facturas F ";
     $sql.= "INNER JOIN inm_alquileres A ON (A.id = F.id_referencia AND F.id_empresa = A.id_empresa) ";
