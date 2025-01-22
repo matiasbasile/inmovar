c2cec8ed9cb9768c340ea9bd8c41ae0836b18672
mbasile <m.basile@avoristravel.com>
2025-01-22 10:57:04 -0300

    feat: proximos contactos

--- templates/correa/includes/footer.php
+++ templates/correa/includes/footer.php
@@ -80,9 +80,62 @@
 <!-- Return to Top -->
 <a href="javascript:" id="return-to-top"><i class="fa fa-chevron-up"></i></a>
 
-<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5><img src="assets/images/whatsapp-icon-2.png" alt="Whatsapp"> enviar whatsapp</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<img src="assets/images/popup-close.png" alt="Close Btn">
+				</button>
+			</div>
+			<div class="modal-body">
+				<form onsubmit="enviar_contacto()">
+					<div class="form-group">
+						<input type="hidden" value="" id="contacto_id_propiedad" name="">
+						<input type="name" name="Nombre *" id="contacto_nombre" placeholder="Nombre *" class="form-control">
+					</div>
+					<div class="form-group">
+						<input type="email" name="Email *" id="contacto_email" placeholder="Email *" class="form-control">
+					</div>
+					<div class="form-group">
+						<input type="tel" name="WhatsApp (sin 0 ni 15) *" id="contacto_telefono" placeholder="WhatsApp (sin 0 ni 15) *" class="form-control">
+					</div>
+					<div class="form-group">
+						<textarea id="contacto_mensaje" placeholder="Estoy interesado en “Duplex en venta en Ringuelet Cod: 1234”" class="form-control"></textarea>
+					</div>
+					<div class="form-group">
+						<button type="submit" id="contacto_submit" class="btn">hablar ahora</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/html5.min.js"></script>
+<script src="assets/js/respond.min.js"></script>
+<script src="assets/js/placeholders.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/slick.min.js"></script>
+<script src="assets/js/scripts.js"></script>
 
 <script type="text/javascript">
+	function llenar_id(item) { 
+		$("#contacto_id_propiedad").val(item);
+	} 
+	function filtrar() { 
+		var link = "<?php echo mklink("propiedades/")?>";
+		var tipo_operacion = $("#tipo_operacion").val();
+		var localidad = $("#localidad").val();
+		var tp = $("#tp").val();
+		link = link + tipo_operacion + "/" + localidad + "/?tp=" + tp;
+		$("#form_propiedades").attr("action",link);
+		return true;
+	}
+
 	function enviar_newsletter() {
   var email = $("#newsletter_email").val();
   if (!validateEmail(email)) {
@@ -117,5 +170,160 @@
   return false;
 }  
 </script>
+
+<script type="text/javascript">
+	function enviar_contacto() {
+
+		var nombre = jQuery("#contacto_nombre").val();
+		var email = jQuery("#contacto_email").val();
+		var mensaje = jQuery("#contacto_mensaje").val();
+		var telefono = jQuery("#contacto_telefono").val();
+		var id_propiedad = jQuery("#contacto_id_propiedad").val();
+
+		if (isEmpty(nombre) || nombre == "Nombre") {
+			alert("Por favor ingrese un nombre");
+			jQuery("#contacto_nombre").focus();
+			return false;          
+		}
+
+
+		if (isEmpty(telefono) || telefono == "telefono") {
+			alert("Por favor ingrese un telefono");
+			jQuery("#contacto_telefono").focus();
+			return false;          
+		}
+
+		if (!validateEmail(email)) {
+			alert("Por favor ingrese un email valido");
+			jQuery("#contacto_email").focus();
+			return false;          
+		}
+		if (isEmpty(mensaje) || mensaje == "Mensaje") {
+			alert("Por favor ingrese un mensaje");
+			jQuery("#contacto_mensaje").focus();
+			return false;              
+		}    
+		jQuery("#contacto_submit").attr('disabled', 'disabled');
+		var datos = {
+			"para":"<?php echo $empresa->email ?>",
+			"nombre":nombre,
+			"telefono":telefono,
+			"email":email,
+			"asunto":"Consulta por propiedad",
+			"mensaje":mensaje,
+			"id_propiedad":id_propiedad,
+			"id_empresa":ID_EMPRESA,
+		}
+		jQuery.ajax({
+			"url":"https://app.inmovar.com/admin/consultas/function/enviar/",
+			"type":"post",
+			"dataType":"json",
+			"data":datos,
+			"success":function(r){
+				if (r.error == 0) {
+					alert("Muchas gracias por contactarse con nosotros. Le responderemos a la mayor brevedad!");
+					window.location.href = "https://api.whatsapp.com/send?phone=549<?php echo $empresa->telefono ?>&text="+ mensaje;
+				} else {
+					alert("Ocurrio un error al enviar su email. Disculpe las molestias");
+					jQuery("#contacto_submit").removeAttr('disabled');
+				}
+			}
+		});
+		return false;
+	}
+</script>
+
+
+<script type="text/javascript">
+  function enviar_contacto_whatsapp() {
+    var nombre = jQuery("#contacto_nombre").val();
+    var email = jQuery("#contacto_email").val();
+    var mensaje = jQuery("#contacto_mensaje").val();
+    var telefono = jQuery("#contacto_telefono").val();
+
+    if (isEmpty(nombre) || nombre == "Nombre") {
+      alert("Por favor ingrese un nombre");
+      jQuery("#contacto_nombre").focus();
+      return false;          
+    }
+
+
+    if (isEmpty(telefono) || telefono == "telefono") {
+      alert("Por favor ingrese un telefono");
+      jQuery("#contacto_telefono").focus();
+      return false;          
+    }
+
+    if (!validateEmail(email)) {
+      alert("Por favor ingrese un email valido");
+      jQuery("#contacto_email").focus();
+      return false;          
+    }
+    if (isEmpty(mensaje) || mensaje == "Mensaje") {
+      alert("Por favor ingrese un mensaje");
+      jQuery("#contacto_mensaje").focus();
+      return false;              
+    }    
+    
+    jQuery("#contacto_submit").attr('disabled', 'disabled');
+    var datos = {
+      "para":"<?php echo $empresa->email ?>",
+      "nombre":nombre,
+      "telefono":telefono,
+      "email":email,
+      "asunto":"Consulta por: <?php echo $propiedad->nombre ?>",
+      "mensaje":mensaje,
+      "id_propiedad":"<?php echo isset($propiedad) ? $propiedad->id : 0 ?>",
+      "id_empresa":ID_EMPRESA,
+    }
+    jQuery.ajax({
+      "url":"https://app.inmovar.com/admin/consultas/function/enviar/",
+      "type":"post",
+      "dataType":"json",
+      "data":datos,
+      "success":function(r){
+        if (r.error == 0) {
+          alert("Muchas gracias por contactarse con nosotros. Le responderemos a la mayor brevedad!");
+          window.location.href = "https://api.whatsapp.com/send?phone=549<?php echo $empresa->telefono ?>&text="+ mensaje;
+        } else {
+          alert("Ocurrio un error al enviar su email. Disculpe las molestias");
+          jQuery("#contacto_submit").removeAttr('disabled');
+        }
+      }
+    });
+    return false;
+  }
+</script>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+    var maximo = 0;
+    $(".product-details h4").each(function(i,e){
+      if ($(e).height() > maximo) maximo = $(e).height();
+    });
+    maximo = Math.ceil(maximo);
+    $(".product-details h4").height(maximo);
+  });
+
+  $(document).ready(function(){
+    var maximo = 0;
+    $(".product-details h5").each(function(i,e){
+      if ($(e).height() > maximo) maximo = $(e).height();
+    });
+    maximo = Math.ceil(maximo);
+    $(".product-details h5").height(maximo);
+  });
+  $(document).ready(function(){
+    var maximo = 0;
+    $(".product-details .average-detail").each(function(i,e){
+     if ($(e).height() > maximo) maximo = $(e).height();
+   });
+    maximo = Math.ceil(maximo);
+    $(".product-details .average-detail").height(maximo);
+  });
+</script>
+
+<script type="text/javascript" src="/admin/resources/js/common.js"></script>
+<script type="text/javascript" src="/admin/resources/js/main.js"></script>
 
 <?php include("templates/comun/clienapp.php") ?>
