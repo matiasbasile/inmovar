0821cdb3a4aeb5766bdc2747b79fda88e050a393
mbasile <m.basile@avoristravel.com>
2025-01-23 18:57:22 -0300

    fix: consultas

--- admin/application/controllers/consultas.php
+++ admin/application/controllers/consultas.php
@@ -29,6 +29,7 @@ class Consultas extends REST_Controller {
     foreach($asuntos as $asunto) {
       $a_asunto[$asunto->id] = array($asunto->id_tipo, $asunto->nombre);
     }
+    print_r($a_asunto);
 
     $sql = "SELECT * FROM clientes WHERE id_empresa = $id_empresa ORDER BY id ASC ";
     $q = $this->db->query($sql);
