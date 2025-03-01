d757dd070e71f55a2a82f96920b364b597feca0d
mbasile <m.basile@avoristravel.com>
2025-03-01 19:53:42 -0300

    fix: grupo urbano

---
+++ templates/grupourbano2/inicio.php
@@ -0,0 +1,27 @@
+<?php
+include_once("includes/init.php");
+$nombre_pagina = "home";
+?>
+<!DOCTYPE html>
+<html dir="ltr" lang="es">
+<head>
+  <?php include("includes/head.php"); ?>
+</head>
+<body>
+  <div class="row">
+    <div class="col-md-6">
+      <img src="assets/images/bertoia.png" />
+      <p>Sucursal 47 Número 881</p>
+      <p>La Plata</p>
+      <a class="btn" href="https://grupourbanobertoia.com.ar">INGRESAR</a>
+    </div>
+    <div class="col-md-6">
+      <img src="assets/images/pinero.png" />
+      <p>Sucursal 532 Número 1265</p>
+      <p>La Plata</p>
+      <a class="btn" href="https://grupourbanobertoia.com.ar">INGRESAR</a>
+    </div>
+  </div>
+
+</body>
+</html>
