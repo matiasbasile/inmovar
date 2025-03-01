4458c4b88b41182b12fd763ca1f4a70030b4547a
mbasile <m.basile@avoristravel.com>
2025-03-01 19:58:20 -0300

    fix: grupo urbano

--- templates/grupourbano2/inicio.php
+++ templates/grupourbano2/inicio.php
@@ -12,17 +12,17 @@ $nombre_pagina = "home";
     <div class="row" style="margin: 200px 0px;">
       <div class="col-md-6">
         <div class="pl30 pr30 tac">
-          <img class="w100p" src="assets/images/bertoia.png" />
-          <p>Sucursal 47 Número 881</p>
-          <p>La Plata</p>
+          <img class="w100p mb30" src="assets/images/bertoia.png" />
+          <p class="fs26 mb15">Sucursal 47 Número 881</p>
+          <p class="fs26 mb15">La Plata</p>
           <a class="btn btn-outline-secondary" href="https://grupourbanobertoia.com.ar">INGRESAR</a>
         </div>
       </div>
       <div class="col-md-6">
         <div class="pl30 pr30 tac">
-          <img class="w100p" src="assets/images/pinero.png" />
-          <p>Sucursal 532 Número 1265</p>
-          <p>La Plata</p>
+          <img class="w100p mb30" src="assets/images/pinero.png" />
+          <p class="fs26 mb15">Sucursal 532 Número 1265</p>
+          <p class="fs26 mb15">La Plata</p>
           <a class="btn btn-outline-secondary" href="https://grupourbanobertoia.com.ar">INGRESAR</a>
         </div>
       </div>
