63f33871c7d89871f3a0606ad8dc68cdd2b031a5
mbasile <m.basile@avoristravel.com>
2025-03-15 11:29:30 -0300

    fix: limpieza

--- admin/application/controllers/limpieza.php
+++ admin/application/controllers/limpieza.php
@@ -10,15 +10,27 @@ class Limpieza extends REST_Controller {
 
   function imagenes() {
     $id_empresa = 45;
-    $base = "uploads/$id_empresa/propiedades/";
-    $sql = "SELECT * FROM inm_propiedades_images WHERE id_empresa = $id_empresa ";
-    $q = $this->db->query($sql);
-    echo "TOTAL DE IMAGENES: ".$q->num_rows()."\n";
     $contador = 0;
     $imagenes = array();
+    $base = "uploads/$id_empresa/propiedades/";
+    
+    // Tabla inm_propiedades_images
+    $sql = "SELECT * FROM inm_propiedades_images WHERE id_empresa = $id_empresa AND path != '' ";
+    $q = $this->db->query($sql);
+    $total_tabla_images = $q->num_rows();
     foreach($q->result() as $image) {
       $imagenes[] = $image->path;
     }
+
+    // Imagen Principal
+    $sql = "SELECT * FROM inm_propiedades WHERE id_empresa = $id_empresa AND path != '' ";
+    $q = $this->db->query($sql);
+    $total_paths = $q->num_rows();
+    foreach($q->result() as $image) {
+      $imagenes[] = $image->path;
+    }
+
+    echo "TOTAL DE IMAGENES: ".($total_tabla_images + $total_paths)."\n";
 
     $para_borrar = array();
 
